7d86cd1590412b3fb019be341172f8d696216463
Constantin Kaplinsky <const@tightvnc.com>
2006-05-11 04:39:38 +0000

    The hooks DLL merged with VNC 4.1.1 code.
    git-svn-id: svn://svn.code.sf.net/p/tigervnc/code/branches/merge-with-vnc-4.1.1@546 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- wm_hooks/wm_hooks.h
+++ wm_hooks/wm_hooks.h
@@ -1,5 +1,5 @@
-/* Copyright (C) 2002-2003 RealVNC Ltd.  All Rights Reserved.
- *    
+/* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
+ * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -27,7 +27,6 @@
 #ifndef __WM_HOOKS_H__
 #define __WM_HOOKS_H__
 
-#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
 #define DLLEXPORT __declspec(dllexport)
