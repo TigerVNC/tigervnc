4fc2026b9595e9425f50616d18781995aebe495b
george82 <george82@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2006-05-24 15:54:52 +0000

    Added the scale controls to the vncviewer options dialog (remove tab symbols in code).
    git-svn-id: svn://svn.code.sf.net/p/tigervnc/code/trunk@588 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- vncviewer/OptionsDialog.cxx
+++ vncviewer/OptionsDialog.cxx
@@ -164,13 +164,13 @@ public:
     setItemChecked(IDC_AUTO_RECONNECT, dlg->options.autoReconnect);
     setItemChecked(IDC_SHOW_TOOLBAR, dlg->options.showToolbar);
     char scale_values[8][20] = {
-		  "25","50","75","90","100","125","150","Auto"
-		};
-		HWND hScaleCombo = GetDlgItem(handle, IDC_COMBO_SCALE);
-		for (int i = 0; i <= 7; i++) {
-		  SendMessage(hScaleCombo, CB_INSERTSTRING, 
+      "25","50","75","90","100","125","150","Auto"
+    };
+    HWND hScaleCombo = GetDlgItem(handle, IDC_COMBO_SCALE);
+    for (int i = 0; i <= 7; i++) {
+      SendMessage(hScaleCombo, CB_INSERTSTRING, 
         (WPARAM)i, (LPARAM)(int FAR*)scale_values[i]);
-		}
+    }
     SetDlgItemText(handle, IDC_COMBO_SCALE, "100");
   }
   virtual bool onOk() {
