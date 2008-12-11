35a70395155c6093dbb7bbe46ffe0f19689601cb
Peter Åstrand <astrand@cendio.se>
2008-12-11 09:08:33 +0000

    Avoid using result variable uninitialized
    git-svn-id: svn://svn.code.sf.net/p/tigervnc/code/trunk@3382 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- win/vncviewer/DesktopWindow.cxx
+++ win/vncviewer/DesktopWindow.cxx
@@ -130,7 +130,7 @@ public:
 };
 
 LRESULT CALLBACK FrameProc(HWND wnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-  LRESULT result;
+  LRESULT result = 0;
   if (msg == WM_CREATE)
     SetWindowLong(wnd, GWL_USERDATA, (long)((CREATESTRUCT*)lParam)->lpCreateParams);
   else if (msg == WM_DESTROY)
