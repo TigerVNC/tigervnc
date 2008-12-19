3d1d86a8e9465c36354ba17dbba7668b961e273d
enikey <enikey@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-12-19 04:32:14 +0000

    [Developement] Added base methods headers to interface RecordInterface.
    git-svn-id: svn://svn.code.sf.net/p/tigervnc/code/trunk@3409 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- java/src/com/tightvnc/vncviewer/RecordInterface.java
+++ java/src/com/tightvnc/vncviewer/RecordInterface.java
@@ -1,9 +1,26 @@
 package com.tightvnc.vncviewer;
 
+import java.io.IOException;
+
 //
 // This interface will be used for recording sessions
 // by decoders classes in com.tighvnc.decoder package.
 //
 public interface RecordInterface {
+  //
+  // Recording state methods
+  //
+  public boolean isRecordFromBeginning();
+  public boolean canWrite();
 
+  //
+  // Write data methods
+  //
+  public void write(byte b[]) throws IOException;
+  public void write(byte b[], int off, int len) throws IOException;
+  public void writeByte(byte b) throws IOException;
+  public void writeByte(int i) throws IOException;
+  public void writeIntBE(int v) throws IOException;
+  public void recordCompactLen(int len) throws IOException;
+  public void recordCompressedData(byte[] data) throws IOException;
 }
