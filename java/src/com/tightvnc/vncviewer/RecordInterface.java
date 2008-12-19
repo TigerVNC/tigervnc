db9fc0ce711b26b8ccc2f3704374665330205e0a
enikey <enikey@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-12-19 04:26:38 +0000

    [Layout] Added RecordInterface interface. This interface will be used for recording sessions by decoders classes in com.tighvnc.decoder package.
    git-svn-id: svn://svn.code.sf.net/p/tigervnc/code/trunk@3408 3789f03b-4d11-0410-bbf8-ca57d06f2519

---
+++ java/src/com/tightvnc/vncviewer/RecordInterface.java
@@ -0,0 +1,9 @@
+package com.tightvnc.vncviewer;
+
+//
+// This interface will be used for recording sessions
+// by decoders classes in com.tighvnc.decoder package.
+//
+public interface RecordInterface {
+
+}
