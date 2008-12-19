0ef32b290f8ce99592980b3476ab27cb952ddc93
enikey <enikey@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-12-19 04:16:34 +0000

    [Developement] Added readFully, readU32, readU8, readCompactLen methods to RfbInputStream class.
    git-svn-id: svn://svn.code.sf.net/p/tigervnc/code/trunk@3406 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- java/src/com/tightvnc/vncviewer/RfbInputStream.java
+++ java/src/com/tightvnc/vncviewer/RfbInputStream.java
@@ -1,8 +1,30 @@
 package com.tightvnc.vncviewer;
 
+import java.io.IOException;
+
 public class RfbInputStream {
   RfbInputStream(RfbProto rfbProto) {
     rfb = rfbProto;
+  }
+
+  public void readFully(byte b[]) throws IOException {
+    readFully(b, 0, b.length);
+  }
+
+  public void readFully(byte b[], int off, int len) throws IOException {
+    rfb.readFully(b, off, len);
+  }
+
+  public int readU32() throws IOException  {
+    return rfb.readU32();
+  }
+
+  public int readU8() throws IOException  {
+    return rfb.readU8();
+  }
+
+  public int readCompactLen() throws IOException {
+    return rfb.readCompactLen();
   }
 
   private RfbProto rfb = null;
