d734f10fe8a752638adbbb151627a10b766ecc40
enikey <enikey@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-12-19 06:28:08 +0000

    [Developement] Added hextile decoder constants and class constructor.
    git-svn-id: svn://svn.code.sf.net/p/tigervnc/code/trunk@3423 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- java/src/com/tightvnc/decoder/HextileDecoder.java
+++ java/src/com/tightvnc/decoder/HextileDecoder.java
@@ -1,5 +1,25 @@
 package com.tightvnc.decoder;
 
-public class HextileDecoder {
+import com.tightvnc.vncviewer.RfbInputStream;
+import java.awt.Graphics;
+
+public class HextileDecoder extends RawDecoder {
+
+  // Contstants used in the Hextile decoder
+  final static int
+    HextileRaw                 = 1,
+    HextileBackgroundSpecified = 2,
+    HextileForegroundSpecified = 4,
+    HextileAnySubrects         = 8,
+    HextileSubrectsColoured    = 16;
+
+  public HextileDecoder(Graphics g, RfbInputStream is) {
+    super(g, is);
+  }
+
+  public HextileDecoder(Graphics g, RfbInputStream is, int frameBufferW,
+                        int frameBufferH) {
+    super(g, is, frameBufferW, frameBufferH);
+  }
 
 }
