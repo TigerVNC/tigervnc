e93c7bc05599598c83130ff792a584973f03b220
enikey <enikey@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-12-24 04:36:37 +0000

    [Developement] Added body to "writeByte" and "writeInt" methods to RecordOutputStream class.
    git-svn-id: svn://svn.code.sf.net/p/tigervnc/code/trunk@3451 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- java/src/com/tightvnc/vncviewer/RecordOutputStream.java
+++ java/src/com/tightvnc/vncviewer/RecordOutputStream.java
@@ -25,13 +25,24 @@ public class RecordOutputStream implements DataOutput {
   }
 
   public void writeBoolean(boolean v) { }
-  public void writeByte(int v) { }
+
+  public void writeByte(int v) throws IOException {
+    if (recordInterface.canWrite()) {
+      recordInterface.writeByte((byte)v);
+    }
+  }
+
   public void writeBytes(String s) { }
   public void writeChar(int v) { }
   public void writeChars(String s) { }
   public void writeDouble(double v) { }
   public void writeFloat(float v) { }
-  public void writeInt(int v) { }
+
+  public void writeInt(int v) throws IOException {
+    if (recordInterface.canWrite())
+      recordInterface.writeIntBE(v);
+  }
+
   public void writeLong(long v) { }
   public void writeShort(int v) { }
   public void writeUTF(String str) { }
