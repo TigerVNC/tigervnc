778f2f0a7855e0366f8922fcd14e76d976ff63f8
Adam Tkac <atkac@redhat.com>
2009-06-12 14:12:25 +0000

    Bump version to 0.0.91.
    git-svn-id: svn://svn.code.sf.net/p/tigervnc/code/trunk@3837 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- unix/xserver/hw/vnc/xvnc.cc
+++ unix/xserver/hw/vnc/xvnc.cc
@@ -84,7 +84,7 @@ extern "C" {
 #undef public
 }
 
-#define XVNCVERSION "TigerVNC 0.0.90"
+#define XVNCVERSION "TigerVNC 0.0.91"
 #define XVNCCOPYRIGHT ("Copyright (C) 2002-2005 RealVNC Ltd.\n" \
 		       "Copyright (C) 2000-2006 Constantin Kaplinsky\n" \
 		       "Copyright (C) 2004-2009 Peter Astrand for Cendio AB\n" \
