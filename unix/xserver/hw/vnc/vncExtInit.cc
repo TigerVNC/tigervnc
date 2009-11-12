881b77c7e7febe4a32cd4ffb72213766b325ff36
Adam Tkac <atkac@redhat.com>
2009-11-12 10:38:13 +0000

    [Bugfix] Define "public" structure members as "c_public" in C includes in vncExtInit.cc (alan dot coopersmith at sun dot com)
    git-svn-id: svn://svn.code.sf.net/p/tigervnc/code/trunk@3920 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- unix/xserver/hw/vnc/vncExtInit.cc
+++ unix/xserver/hw/vnc/vncExtInit.cc
@@ -24,6 +24,7 @@
 
 extern "C" {
 #define class c_class
+#define public c_public
 #define NEED_EVENTS
 #include <X11/X.h>
 #include <X11/Xproto.h>
@@ -38,6 +39,7 @@ extern "C" {
 #include "vncExt.h"
 #undef class
 #undef xalloc
+#undef public
 }
 
 #include <rfb/Configuration.h>
