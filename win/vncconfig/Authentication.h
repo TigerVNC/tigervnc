a35b500a23ef129eafed2db2fc932132a7f61968
Adam Tkac <atkac@redhat.com>
2010-09-15 11:37:50 +0000

    [Bugfix] Fix vncconfig.exe compilation (broken since r4123).
    git-svn-id: svn://svn.code.sf.net/p/tigervnc/code/trunk@4132 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- win/vncconfig/Authentication.h
+++ win/vncconfig/Authentication.h
@@ -40,11 +40,11 @@ namespace rfb {
       AuthenticationPage(const RegKey& rk)
         : PropSheetPage(GetModuleHandle(0), MAKEINTRESOURCE(IDD_AUTHENTICATION)), regKey(rk) {}
       void initDialog() {
-        CharArray sec_types_str(Security::secTypes.getData());
-        std::list<rdr::U8> sec_types = parseSecTypes(sec_types_str.buf);
+        CharArray sec_types_str(Security::secTypesServer.getData());
+        std::list<rdr::U32> sec_types = parseSecTypes(sec_types_str.buf);
 
         useNone = useVNC = false;
-        std::list<rdr::U8>::iterator i;
+        std::list<rdr::U32>::iterator i;
         for (i=sec_types.begin(); i!=sec_types.end(); i++) {
           if ((*i) == secTypeNone) useNone = true;
           else if ((*i) == secTypeVncAuth) useVNC = true;
