3b91a8e3b636dc92e00ece7b06b99abb5b6dc300
Adam Tkac <atkac@redhat.com>
2010-11-11 14:49:12 +0000

    [Doc] Add copyrights to recently added Message Box code.
    git-svn-id: svn://svn.code.sf.net/p/tigervnc/code/trunk@4196 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- java/src/com/tigervnc/vncviewer/MessageBox.java
+++ java/src/com/tigervnc/vncviewer/MessageBox.java
@@ -1,4 +1,5 @@
 /* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
+ * Copyright (C) 2010 TigerVNC Team
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
