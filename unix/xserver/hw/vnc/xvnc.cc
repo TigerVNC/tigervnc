a2f5f1a662633021ce53c480d52e50ef5b2da861
DRC <dcommander@users.sourceforge.net>
2011-12-23 14:13:04 +0000

    1.1.90 (1.2 beta1)
    git-svn-id: svn://svn.code.sf.net/p/tigervnc/code/trunk@4826 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- unix/xserver/hw/vnc/xvnc.cc
+++ unix/xserver/hw/vnc/xvnc.cc
@@ -96,7 +96,7 @@ extern "C" {
 #define Xfree free
 #endif
 
-#define XVNCVERSION "TigerVNC 1.1.80"
+#define XVNCVERSION "TigerVNC 1.1.90"
 #define XVNCCOPYRIGHT ("Copyright (C) 1999-2011 TigerVNC Team and many others (see README.txt)\n" \
                        "See http://www.tigervnc.org for information on TigerVNC.\n")
 
