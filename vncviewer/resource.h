ca937761f5e6cd915201d0691f6383fe52316c58
Pierre Ossman <ossman@cendio.se>
2012-03-27 12:30:19 +0000

    Add a resource file to vncviewer.exe so we can get an icon.
    git-svn-id: svn://svn.code.sf.net/p/tigervnc/code/trunk@4876 3789f03b-4d11-0410-bbf8-ca57d06f2519

---
+++ vncviewer/resource.h
@@ -0,0 +1,7 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by vncviewer.rc
+//
+#define IDR_MANIFEST                    1
+#define IDI_ICON                        101
+
