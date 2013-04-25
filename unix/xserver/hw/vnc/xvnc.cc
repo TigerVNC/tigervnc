b5a652cfb37254e26ede0f2a86e8259a073113eb
Pierre Ossman <ossman@cendio.se>
2013-04-25 07:55:46 +0000

    Bump version to 1.2.90 (1.3beta1).
    git-svn-id: svn://svn.code.sf.net/p/tigervnc/code/branches/1_3@5086 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- unix/xserver/hw/vnc/xvnc.cc
+++ unix/xserver/hw/vnc/xvnc.cc
@@ -98,7 +98,7 @@ extern "C" {
 #define Xfree free
 #endif
 
-#define XVNCVERSION "TigerVNC 1.2.80"
+#define XVNCVERSION "TigerVNC 1.2.90"
 #define XVNCCOPYRIGHT ("Copyright (C) 1999-2011 TigerVNC Team and many others (see README.txt)\n" \
                        "See http://www.tigervnc.org for information on TigerVNC.\n")
 
