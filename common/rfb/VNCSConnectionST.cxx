TigerVNC/tigervnc

commit b64dbf29aeaaa41eed47ea48e3ccfa9ae563c6f2

Avoid losing updates on partial requests
A request may be for only part of the frame buffer, meaning we cannot
discard all changes just because we've send out an update. There might
still be modified areas remaining that haven't been requested yet.

common/rfb/VNCSConnectionST.cxx
@@ -1056,8 +1056,11 @@ void VNCSConnectionST::writeFramebufferUpdate()
 
     writeRTTPing();
 
+    // The request might be for just part of the screen, so we cannot
+    // just clear the entire update tracker.
+    updates.subtract(requested);
+
     requested.clear();
-    updates.clear();
   }
 
 out:

commit 3c56d4f3c7fc2d706acd114d1abb04e9a338dbea

Fix infinite update caused by b64dbf2
The bug fix in b64dbf2 didn't account for the proper request
region in the case of continuous updates. Make sure we use the
proper variable for which region we've sent updates for.

common/rfb/VNCSConnectionST.cxx
@@ -1058,7 +1058,7 @@ void VNCSConnectionST::writeFramebufferUpdate()
 
     // The request might be for just part of the screen, so we cannot
     // just clear the entire update tracker.
-    updates.subtract(requested);
+    updates.subtract(req);
 
     requested.clear();
   }