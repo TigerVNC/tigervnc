TigerVNC/tigervnc

commit f8e3b34c69b848781b958c7a4d21175ddc6f90d8

Restructure Xvnc/libvnc.so code to avoid C++ header hacks
The internal Xorg headers are very incompatible with C++ and we've had
to resort to all kinds of hacks in order to include them in our C++
code. This approach isn't really viable long term so restructure things
so that we have a glue layer written in C that bridges the Xorg core
with the RFB classes.

unix/xserver/hw/vnc/vncBlockHandler.c
@@ -1,0 +1,143 @@
+/* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
+ * Copyright 2011-2014 Pierre Ossman for Cendio AB
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+ * USA.
+ */
+
+#ifdef HAVE_DIX_CONFIG_H
+#include <dix-config.h>
+#endif
+
+#include <errno.h>
+
+#include <X11/Xpoll.h>
+
+#include "dix.h"
+#include "scrnintstr.h"
+
+#include "vncExtInit.h"
+#include "vncBlockHandler.h"
+
+static void vncBlockHandler(void * data, OSTimePtr t, void * readmask);
+static void vncWakeupHandler(void * data, int nfds, void * readmask);
+void vncWriteBlockHandler(fd_set *fds);
+void vncWriteWakeupHandler(int nfds, fd_set *fds);
+
+void vncRegisterBlockHandlers(void)
+{
+  if (!RegisterBlockAndWakeupHandlers(vncBlockHandler, vncWakeupHandler, 0))
+    FatalError("RegisterBlockAndWakeupHandlers() failed\n");
+}
+
+static void vncWriteBlockHandlerFallback(OSTimePtr timeout);
+static void vncWriteWakeupHandlerFallback(void);
+
+//
+// vncBlockHandler - called just before the X server goes into select().  Call
+// on to the block handler for each desktop.  Then check whether any of the
+// selections have changed, and if so, notify any interested X clients.
+//
+
+static void vncBlockHandler(void * data, OSTimePtr timeout, void * readmask)
+{
+  fd_set* fds = (fd_set*)readmask;
+
+  vncWriteBlockHandlerFallback(timeout);
+
+  vncCallReadBlockHandlers(fds, timeout);
+}
+
+static void vncWakeupHandler(void * data, int nfds, void * readmask)
+{
+  fd_set* fds = (fd_set*)readmask;
+
+  vncCallReadWakeupHandlers(fds, nfds);
+
+  vncWriteWakeupHandlerFallback();
+}
+
+//
+// vncWriteBlockHandler - extra hack to be able to get the main select loop
+// to monitor writeable fds and not just readable. This requirers a modified
+// Xorg and might therefore not be called. When it is called though, it will
+// do so before vncBlockHandler (and vncWriteWakeupHandler called after
+// vncWakeupHandler).
+//
+
+static Bool needFallback = TRUE;
+static fd_set fallbackFds;
+static struct timeval tw;
+
+void vncWriteBlockHandler(fd_set *fds)
+{
+  struct timeval *dummy;
+
+  needFallback = FALSE;
+
+  dummy = NULL;
+  vncCallWriteBlockHandlers(fds, &dummy);
+}
+
+void vncWriteWakeupHandler(int nfds, fd_set *fds)
+{
+  vncCallWriteWakeupHandlers(fds, nfds);
+}
+
+static void vncWriteBlockHandlerFallback(OSTimePtr timeout)
+{
+  if (!needFallback)
+    return;
+
+  FD_ZERO(&fallbackFds);
+  vncWriteBlockHandler(&fallbackFds);
+
+  if (!XFD_ANYSET(&fallbackFds))
+    return;
+
+  if ((*timeout == NULL) ||
+      ((*timeout)->tv_sec > 0) || ((*timeout)->tv_usec > 10000)) {
+    tw.tv_sec = 0;
+    tw.tv_usec = 10000;
+    *timeout = &tw;
+  }
+}
+
+static void vncWriteWakeupHandlerFallback(void)
+{
+  int ret;
+  struct timeval timeout;
+
+  if (!needFallback)
+    return;
+
+  if (!XFD_ANYSET(&fallbackFds))
+    return;
+
+  timeout.tv_sec = 0;
+  timeout.tv_usec = 0;
+
+  ret = select(XFD_SETSIZE, NULL, &fallbackFds, NULL, &timeout);
+  if (ret < 0) {
+    ErrorF("vncWriteWakeupHandlerFallback(): select: %s\n",
+           strerror(errno));
+    return;
+  }
+
+  if (ret == 0)
+    return;
+
+  vncWriteWakeupHandler(ret, &fallbackFds);
+}

common/rfb/ServerCore.cxx
@@ -97,4 +97,8 @@ rfb::BoolParameter rfb::Server::queryConnect
 ("QueryConnect",
  "Prompt the local user to accept or reject incoming connections.",
  false);
-
+rfb::IntParameter rfb::Server::queryConnectTimeout
+("QueryConnectTimeout",
+ "Number of seconds to show the Accept Connection dialog before "
+ "rejecting the connection",
+ 10);

common/rfb/ServerCore.h
@@ -48,6 +48,7 @@ namespace rfb {
     static BoolParameter sendCutText;
     static BoolParameter acceptSetDesktopSize;
     static BoolParameter queryConnect;
+    static IntParameter queryConnectTimeout;
 
   };
 

common/rfb/VNCSConnectionST.cxx
@@ -1,5 +1,5 @@
 /* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
- * Copyright 2009-2014 Pierre Ossman for Cendio AB
+ * Copyright 2009-2015 Pierre Ossman for Cendio AB
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -66,7 +66,8 @@ struct RTTInfo {
 
 VNCSConnectionST::VNCSConnectionST(VNCServerST* server_, network::Socket *s,
                                    bool reverse)
-  : SConnection(reverse), sock(s), inProcessMessages(false),
+  : SConnection(reverse), sock(s),
+    queryConnectTimer(this), inProcessMessages(false),
     pendingSyncFence(false), syncFence(false), fenceFlags(0),
     fenceDataLen(0), fenceData(NULL),
     baseRTT(-1), minRTT(-1), seenCongestion(false), pingCounter(0),
@@ -434,8 +435,10 @@ void VNCSConnectionST::queryConnection(const char* userName)
   CharArray reason;
   VNCServerST::queryResult qr = server->queryConnection(sock, userName,
                                                         &reason.buf);
-  if (qr == VNCServerST::PENDING)
+  if (qr == VNCServerST::PENDING) {
+    queryConnectTimer.start(rfb::Server::queryConnectTimeout * 1000);
     return;
+  }
 
   // - If server returns ACCEPT/REJECT then pass result to SConnection
   approveConnection(qr == VNCServerST::ACCEPT, reason.buf);
@@ -714,6 +717,10 @@ bool VNCSConnectionST::handleTimeout(Timer* t)
       writeFramebufferUpdate();
     else if (t == &congestionTimer)
       updateCongestion();
+    else if (t == &queryConnectTimer) {
+      if (state() == RFBSTATE_QUERYING)
+        approveConnection(false, "The attempt to prompt the user to accept the connection failed");
+    }
   } catch (rdr::Exception& e) {
     close(e.str());
   }

common/rfb/VNCSConnectionST.h
@@ -174,6 +174,8 @@ namespace rfb {
     network::Socket* sock;
     CharArray peerEndpoint;
 
+    Timer queryConnectTimer;
+
     bool inProcessMessages;
 
     bool pendingSyncFence, syncFence;

unix/xserver/hw/vnc/Input.h
@@ -1,7 +1,7 @@
 /* Copyright (C) 2009 TightVNC Team
  * Copyright (C) 2009, 2010 Red Hat, Inc.
  * Copyright (C) 2009, 2010 TigerVNC Team
- * Copyright 2013 Pierre Ossman for Cendio AB
+ * Copyright 2013-2015 Pierre Ossman for Cendio AB
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,109 +23,44 @@
 #ifndef INPUT_H_
 #define INPUT_H_
 
-#ifdef HAVE_DIX_CONFIG_H
-#include <dix-config.h>
-#endif
+#include <stdlib.h>
+#include <X11/X.h>
 
-#include <list>
-
-#include <rdr/types.h>
-#include <rfb/Rect.h>
-
+#ifdef __cplusplus
 extern "C" {
-#include "input.h"
-/* The Xorg headers define macros that wreak havoc with STL */
-#undef max
-};
-
-#include "xorg-version.h"
-
-/*
- * Represents input device (keyboard + pointer)
- *
- * Is a singleton as input devices are global in the X server so
- * we do not have one per desktop (i.e. per screen).
- */
-extern class InputDevice *vncInputDevice;
-
-class InputDevice {
-public:
-	/*
-	 * Press or release buttons. Relationship between buttonMask and
-	 * buttons is specified in RFB protocol.
-	 */
-	void PointerButtonAction(int buttonMask);
-
-	/* Move pointer to target location (point coords are absolute). */
-	void PointerMove(const rfb::Point &point);
-
-	/* Get current known location of the pointer */
-	const rfb::Point &getPointerPos(void);
-
-	/* Press or release one or more keys to get the given symbol */
-	void KeyboardPress(rdr::U32 keysym) { keyEvent(keysym, true); }
-	void KeyboardRelease(rdr::U32 keysym) { keyEvent(keysym, false); }
-
-	/*
-	 * Init input device.
-	 * This has to be called after core pointer/keyboard
-	 * initialization which unfortunately is after extesions
-	 * initialization (which means we cannot call it in
-	 * vncExtensionInit(). Check InitExtensions(),
-	 * InitCoreDevices() and InitInput() calls in dix/main.c.
-	 * Instead we call it from XserverDesktop at an appropriate
-	 * time.
-	 */
-	void InitInputDevice(void);
-
-private:
-	InputDevice();
-
-	void keyEvent(rdr::U32 keysym, bool down);
-
-	/* Backend dependent functions below here */
-	void PrepareInputDevices(void);
-
-	unsigned getKeyboardState(void);
-	unsigned getLevelThreeMask(void);
-
-	KeyCode pressShift(void);
-	std::list<KeyCode> releaseShift(void);
-
-	KeyCode pressLevelThree(void);
-	std::list<KeyCode> releaseLevelThree(void);
-
-	KeyCode keysymToKeycode(KeySym keysym, unsigned state, unsigned *new_state);
-
-	bool isLockModifier(KeyCode keycode, unsigned state);
-
-	bool isAffectedByNumLock(KeyCode keycode);
-
-	KeyCode addKeysym(KeySym keysym, unsigned state);
-
-private:
-	static int pointerProc(DeviceIntPtr pDevice, int onoff);
-	static int keyboardProc(DeviceIntPtr pDevice, int onoff);
-
-#if XORG >= 17
-	static void vncXkbProcessDeviceEvent(int screenNum,
-	                                     InternalEvent *event,
-	                                     DeviceIntPtr dev);
-#else
-	static void GetInitKeyboardMap(KeySymsPtr keysyms, CARD8 *modmap);
 #endif
 
-private:
-	DeviceIntPtr keyboardDev;
-	DeviceIntPtr pointerDev;
+void vncInitInputDevice(void);
 
-	int oldButtonMask;
-	rfb::Point cursorPos;
+void vncPointerButtonAction(int buttonMask);
+void vncPointerMove(int x, int y);
+void vncGetPointerPos(int *x, int *y);
 
-	KeySym pressedKeys[256];
+void vncKeyboardEvent(KeySym keysym, int down);
 
-private:
-	static InputDevice singleton;
-};
+/* Backend dependent functions below here */
+
+void vncPrepareInputDevices(void);
+
+unsigned vncGetKeyboardState(void);
+unsigned vncGetLevelThreeMask(void);
+
+KeyCode vncPressShift(void);
+size_t vncReleaseShift(KeyCode *keys, size_t maxKeys);
+
+KeyCode vncPressLevelThree(void);
+size_t vncReleaseLevelThree(KeyCode *keys, size_t maxKeys);
+
+KeyCode vncKeysymToKeycode(KeySym keysym, unsigned state, unsigned *new_state);
+
+int vncIsLockModifier(KeyCode keycode, unsigned state);
+
+int vncIsAffectedByNumLock(KeyCode keycode);
+
+KeyCode vncAddKeysym(KeySym keysym, unsigned state);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif

unix/xserver/hw/vnc/InputCore.c
@@ -1,6 +1,6 @@
 /* Copyright (C) 2009 TightVNC Team
  * Copyright (C) 2009 Red Hat, Inc.
- * Copyright 2013 Pierre Ossman for Cendio AB
+ * Copyright 2013-2015 Pierre Ossman for Cendio AB
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,9 +25,6 @@
 #include "Input.h"
 #include "xorg-version.h"
 
-extern "C" {
-#define public c_public
-#define class c_class
 #include "inputstr.h"
 #ifndef XKB_IN_SERVER
 #define XKB_IN_SERVER
@@ -46,9 +43,8 @@ extern "C" {
 #include <X11/XF86keysym.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
-#undef public
-#undef class
-}
+
+extern DeviceIntPtr vncKeyboardDev;
 
 #if XORG < 17
 
@@ -61,7 +57,7 @@ extern "C" {
 #define MAX_KEY 255
 #define MAP_LEN (MAX_KEY - MIN_KEY + 1)
 #define KEYSYMS_PER_KEY 2
-KeySym keyboardMap[MAP_LEN * KEYSYMS_PER_KEY] = {
+static KeySym keyboardMap[MAP_LEN * KEYSYMS_PER_KEY] = {
 	NoSymbol, NoSymbol,
 	XK_Escape, NoSymbol,
 	XK_1, XK_exclam,
@@ -174,7 +170,7 @@ KeySym keyboardMap[MAP_LEN * KEYSYMS_PER_KEY] = {
 	XK_Menu, NoSymbol,
 };
 
-void InputDevice::GetInitKeyboardMap(KeySymsPtr keysyms, CARD8 *modmap)
+void vncGetInitKeyboardMap(KeySymsPtr keysyms, CARD8 *modmap)
 {
 	int i;
 
@@ -221,17 +217,17 @@ void InputDevice::GetInitKeyboardMap(KeySymsPtr keysyms, CARD8 *modmap)
 	keysyms->map = keyboardMap;
 }
 
-void InputDevice::PrepareInputDevices(void)
+void vncPrepareInputDevices(void)
 {
 	/* Don't need to do anything here */
 }
 
-unsigned InputDevice::getKeyboardState(void)
+unsigned vncGetKeyboardState(void)
 {
-	return keyboardDev->key->state;
+	return vncKeyboardDev->key->state;
 }
 
-unsigned InputDevice::getLevelThreeMask(void)
+unsigned vncGetLevelThreeMask(void)
 {
 	int i, j, k;
 
@@ -241,12 +237,12 @@ unsigned InputDevice::getLevelThreeMask(void)
 	int maxKeysPerMod;
 	CARD8 *modmap;
 
-	minKeyCode = keyboardDev->key->curKeySyms.minKeyCode;
-	mapWidth = keyboardDev->key->curKeySyms.mapWidth;
-	map = keyboardDev->key->curKeySyms.map;
+	minKeyCode = vncKeyboardDev->key->curKeySyms.minKeyCode;
+	mapWidth = vncKeyboardDev->key->curKeySyms.mapWidth;
+	map = vncKeyboardDev->key->curKeySyms.map;
 
-	maxKeysPerMod = keyboardDev->key->maxKeysPerModifier;
-	modmap = keyboardDev->key->modifierKeyMap;
+	maxKeysPerMod = vncKeyboardDev->key->maxKeysPerModifier;
+	modmap = vncKeyboardDev->key->modifierKeyMap;
 
 	for (i = 3; i < 8; i++) {
 		for (k = 0; k < maxKeysPerMod; k++) {
@@ -268,41 +264,44 @@ unsigned InputDevice::getLevelThreeMask(void)
 	return 0;
 }
 
-KeyCode InputDevice::pressShift(void)
+KeyCode vncPressShift(void)
 {
 	int maxKeysPerMod;
 
-	maxKeysPerMod = keyboardDev->key->maxKeysPerModifier;
-	return keyboardDev->key->modifierKeyMap[ShiftMapIndex * maxKeysPerMod];
+	maxKeysPerMod = vncKeyboardDev->key->maxKeysPerModifier;
+	return vncKeyboardDev->key->modifierKeyMap[ShiftMapIndex * maxKeysPerMod];
 }
 
-std::list<KeyCode> InputDevice::releaseShift(void)
+size_t vncReleaseShift(KeyCode *keys, size_t maxKeys)
 {
-	std::list<KeyCode> keys;
-
+	size_t count;
 	int maxKeysPerMod;
 
-	maxKeysPerMod = keyboardDev->key->maxKeysPerModifier;
+	count = 0;
+	maxKeysPerMod = vncKeyboardDev->key->maxKeysPerModifier;
 	for (int k = 0; k < maxKeysPerMod; k++) {
 		int keycode;
 		int index;
 
 		index = ShiftMapIndex * maxKeysPerMod + k;
 
-		keycode = keyboardDev->key->modifierKeyMap[index];
+		keycode = vncKeyboardDev->key->modifierKeyMap[index];
 		if (keycode == 0)
 			continue;
 
-		if (!IS_PRESSED(keyboardDev, keycode))
+		if (!IS_PRESSED(vncKeyboardDev, keycode))
 			continue;
 
-		keys.push_back(keycode);
+		if (count >= maxKeys)
+			return 0;
+
+		keys[count++] = keycode;
 	}
 
-	return keys;
+	return count;
 }
 
-KeyCode InputDevice::pressLevelThree(void)
+KeyCode vncPressLevelThree(void)
 {
 	unsigned mask, index;
 	int maxKeysPerMod;
@@ -312,41 +311,44 @@ KeyCode InputDevice::pressLevelThree(void)
 		return 0;
 
 	index = ffs(mask) - 1;
-	maxKeysPerMod = keyboardDev->key->maxKeysPerModifier;
-	return keyboardDev->key->modifierKeyMap[index * maxKeysPerMod];
+	maxKeysPerMod = vncKeyboardDev->key->maxKeysPerModifier;
+	return vncKeyboardDev->key->modifierKeyMap[index * maxKeysPerMod];
 }
 
-std::list<KeyCode> InputDevice::releaseLevelThree(void)
+size_t vncReleaseLevelThree(KeyCode *keys, size_t maxKeys)
 {
-	std::list<KeyCode> keys;
-
+	size_t count;
 	unsigned mask, msindex;
 	int maxKeysPerMod;
 
 	mask = getLevelThreeMask();
 	if (mask == 0)
-		return keys;
+		return 0;
 
 	msindex = ffs(mask) - 1;
 
-	maxKeysPerMod = keyboardDev->key->maxKeysPerModifier;
+	count = 0;
+	maxKeysPerMod = vncKeyboardDev->key->maxKeysPerModifier;
 	for (int k = 0; k < maxKeysPerMod; k++) {
 		int keycode;
 		int index;
 
 		index = msindex * maxKeysPerMod + k;
 
-		keycode = keyboardDev->key->modifierKeyMap[index];
+		keycode = vncKeyboardDev->key->modifierKeyMap[index];
 		if (keycode == 0)
 			continue;
 
-		if (!IS_PRESSED(keyboardDev, keycode))
+		if (!IS_PRESSED(vncKeyboardDev, keycode))
 			continue;
 
-		keys.push_back(keycode);
+		if (count >= maxKeys)
+			return 0;
+
+		keys[count++] = keycode;
 	}
 
-	return keys;
+	return count;
 }
 
 static KeySym KeyCodetoKeySym(KeySymsPtr keymap, int keycode, int col)
@@ -397,7 +399,7 @@ static KeySym KeyCodetoKeySym(KeySymsPtr keymap, int keycode, int col)
 	return syms[col];
 }
 
-KeyCode InputDevice::keysymToKeycode(KeySym keysym, unsigned state, unsigned *new_state)
+KeyCode vncKeysymToKeycode(KeySym keysym, unsigned state, unsigned *new_state)
 {
 	int i, j;
 	unsigned mask;
@@ -405,9 +407,9 @@ KeyCode InputDevice::keysymToKeycode(KeySym keysym, unsigned state, unsigned *ne
 	KeySymsPtr keymap;
 	int mapWidth;
 
-	mask = getLevelThreeMask();
+	mask = vncGetLevelThreeMask();
 
-	keymap = &keyboardDev->key->curKeySyms;
+	keymap = &vncKeyboardDev->key->curKeySyms;
 
 	/*
 	 * Column 0 means both shift and "mode_switch" (AltGr) must be released,
@@ -449,7 +451,7 @@ KeyCode InputDevice::keysymToKeycode(KeySym keysym, unsigned state, unsigned *ne
 	return 0;
 }
 
-bool InputDevice::isLockModifier(KeyCode keycode, unsigned state)
+int vncIsLockModifier(KeyCode keycode, unsigned state)
 {
 	int i, j, k;
 
@@ -461,12 +463,12 @@ bool InputDevice::isLockModifier(KeyCode keycode, unsigned state)
 
 	int num_lock_index;
 
-	minKeyCode = keyboardDev->key->curKeySyms.minKeyCode;
-	mapWidth = keyboardDev->key->curKeySyms.mapWidth;
-	map = keyboardDev->key->curKeySyms.map;
+	minKeyCode = vncKeyboardDev->key->curKeySyms.minKeyCode;
+	mapWidth = vncKeyboardDev->key->curKeySyms.mapWidth;
+	map = vncKeyboardDev->key->curKeySyms.map;
 
-	maxKeysPerMod = keyboardDev->key->maxKeysPerModifier;
-	modmap = keyboardDev->key->modifierKeyMap;
+	maxKeysPerMod = vncKeyboardDev->key->maxKeysPerModifier;
+	modmap = vncKeyboardDev->key->modifierKeyMap;
 
 	/* Caps Lock is fairly easy as it has a dedicated modmap entry */
 	for (k = 0; k < maxKeysPerMod; k++) {
@@ -474,7 +476,7 @@ bool InputDevice::isLockModifier(KeyCode keycode, unsigned state)
 
 		index = LockMapIndex * maxKeysPerMod + k;
 		if (keycode == modmap[index])
-			return true;
+			return 1;
 	}
 
 	/* For Num Lock we need to find the correct modmap entry */
@@ -500,7 +502,7 @@ bool InputDevice::isLockModifier(KeyCode keycode, unsigned state)
 done:
 
 	if (num_lock_index == 0)
-		return false;
+		return 0;
 
 	/* Now we can look in the modmap */ 
 	for (k = 0; k < maxKeysPerMod; k++) {
@@ -508,43 +510,43 @@ done:
 
 		index = num_lock_index * maxKeysPerMod + k;
 		if (keycode == modmap[index])
-			return true;
+			return 1;
 	}
 
-	return false;
+	return 0;
 }
 
-bool InputDevice::isAffectedByNumLock(KeyCode keycode)
+int vncIsAffectedByNumLock(KeyCode keycode)
 {
 	KeySymsPtr keymap;
 	int i, per;
 	KeySym *syms;
 
-	keymap = &keyboardDev->key->curKeySyms;
+	keymap = &vncKeyboardDev->key->curKeySyms;
 	per = keymap->mapWidth;
 	syms = &keymap->map[(keycode - keymap->minKeyCode) * per];
 
 	for (i = 0;i < per;i++) {
 		if (IsKeypadKey(syms[i]))
-			return true;
+			return 1;
 		if (IsPrivateKeypadKey(syms[i]))
-			return true;
+			return 1;
 	}
 
-	return false;
+	return 0;
 }
 
-KeyCode InputDevice::addKeysym(KeySym keysym, unsigned state)
+KeyCode vncAddKeysym(KeySym keysym, unsigned state)
 {
 	KeyCode kc;
 
 	int minKeyCode, maxKeyCode, mapWidth;
 	KeySym *map;
 
-	minKeyCode = keyboardDev->key->curKeySyms.minKeyCode;
-	maxKeyCode = keyboardDev->key->curKeySyms.maxKeyCode;
-	mapWidth = keyboardDev->key->curKeySyms.mapWidth;
-	map = keyboardDev->key->curKeySyms.map;
+	minKeyCode = vncKeyboardDev->key->curKeySyms.minKeyCode;
+	maxKeyCode = vncKeyboardDev->key->curKeySyms.maxKeyCode;
+	mapWidth = vncKeyboardDev->key->curKeySyms.mapWidth;
+	map = vncKeyboardDev->key->curKeySyms.map;
 
 	/*
 	 * Magic, which dynamically adds keysym<->keycode mapping
@@ -570,17 +572,17 @@ KeyCode InputDevice::addKeysym(KeySym keysym, unsigned state)
 #if XORG == 15
 		master = inputInfo.keyboard;
 #else
-		master = keyboardDev->u.master;
+		master = vncKeyboardDev->u.master;
 #endif
 		void *slave = dixLookupPrivate(&master->devPrivates,
 					       CoreDevicePrivateKey);
-		if (keyboardDev == slave) {
+		if (vncKeyboardDev == slave) {
 			dixSetPrivate(&master->devPrivates,
 				      CoreDevicePrivateKey, NULL);
 #if XORG == 15
-			SwitchCoreKeyboard(keyboardDev);
+			SwitchCoreKeyboard(vncKeyboardDev);
 #else
-			CopyKeyClass(keyboardDev, master);
+			CopyKeyClass(vncKeyboardDev, master);
 #endif
 		}
 

commit c88dc805e55cffa7af83ae741b0ffa26aade068e

Make sure fallback block handler remains enabled
Commit f8e3b34 introduced a regression where the fallback write block
handler would cease working after the first time it was called,
potentially stalling writes.

unix/xserver/hw/vnc/vncBlockHandler.c
@@ -104,6 +104,9 @@ static void vncWriteBlockHandlerFallback(OSTimePtr timeout)
   FD_ZERO(&fallbackFds);
   vncWriteBlockHandler(&fallbackFds);
 
+  // vncWriteBlockHandler() will clear this, so we need to restore it
+  needFallback = TRUE;
+
   if (!XFD_ANYSET(&fallbackFds))
     return;
 