4ae358d561463f1a2dc763fec885be5e4932f394
Pierre Ossman <ossman@cendio.se>
2016-02-05 10:28:49 +0100

    Remove no longer relevant license comment

--- common/rfb/util.cxx
+++ common/rfb/util.cxx
@@ -16,20 +16,6 @@
  * USA.
  */
 
-/*
- * The following applies to stcasecmp and strncasecmp implementations:
- *
- * Copyright (c) 1987 Regents of the University of California.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms are permitted
- * provided that this notice is preserved and that due credit is given
- * to the University of California at Berkeley. The name of the University
- * may not be used to endorse or promote products derived from this
- * software without specific written prior permission. This software
- * is provided ``as is'' without express or implied warranty.
- */
-
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
