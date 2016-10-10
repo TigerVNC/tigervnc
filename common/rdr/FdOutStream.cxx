TigerVNC/tigervnc

commit c6df31db54bffe76ee5506a79f42cc7de7c541c3

Clean up FdOutStream::flush()
The logic was a bit confusing and superfluous.

common/rdr/FdOutStream.cxx
@@ -95,32 +95,14 @@ unsigned FdOutStream::getIdleTime()
 
 void FdOutStream::flush()
 {
-  int timeoutms_;
-
-  if (blocking)
-    timeoutms_ = timeoutms;
-  else
-    timeoutms_ = 0;
-
   while (sentUpTo < ptr) {
     int n = writeWithTimeout((const void*) sentUpTo,
-                             ptr - sentUpTo, timeoutms_);
+                             ptr - sentUpTo,
+                             blocking? timeoutms : 0);
 
     // Timeout?
-    if (n == 0) {
-      // If non-blocking then we're done here
-      if (!blocking)
-        break;
-
-      // Otherwise try blocking (with possible timeout)
-      if ((timeoutms_ == 0) && (timeoutms != 0)) {
-        timeoutms_ = timeoutms;
-        break;
-      }
-
-      // Proper timeout
+    if ((n == 0) && blocking)
       throw TimedOut();
-    }
 
     sentUpTo += n;
     offset += n;

commit b08b3d42d92e23b5712fec0da1ba784d8b26e2e4

Fix busy loop in FdOutStream::flush()
This bug was introduced in c6df31db. A non-blocking socket that did
not have any more space would busy loop until the write succeeded.
Instead now it returns without any action, just as it did before
the bug was introduced.

common/rdr/FdOutStream.cxx
@@ -101,8 +101,13 @@ void FdOutStream::flush()
                              blocking? timeoutms : 0);
 
     // Timeout?
-    if ((n == 0) && blocking)
+    if (n == 0) {
+      // If non-blocking then we're done here
+      if (!blocking)
+        break;
+
       throw TimedOut();
+    }
 
     sentUpTo += n;
     offset += n;