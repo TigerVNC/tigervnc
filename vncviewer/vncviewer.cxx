8c2a744c23dedcfdf07682949d46cb70c0f076e4
Pierre Ossman <ossman@cendio.se>
2017-11-14 16:04:32 +0100

    Handle FLTK arguments at any position
    E.g. vncviewer server.example.com:2 -display :2

--- vncviewer/vncviewer.cxx
+++ vncviewer/vncviewer.cxx
@@ -493,26 +493,31 @@ int main(int argc, char** argv)
     if (alertOnFatalError)
       fl_alert("%s", e.str());
   }
-  
-  int i = 1;
-  if (!Fl::args(argc, argv, i) || i < argc)
-    for (; i < argc; i++) {
-      if (Configuration::setParam(argv[i]))
-        continue;
 
-      if (argv[i][0] == '-') {
-        if (i+1 < argc) {
-          if (Configuration::setParam(&argv[i][1], argv[i+1])) {
-            i++;
-            continue;
-          }
+  for (int i = 1; i < argc;) {
+    if (Fl::arg(argc, argv, i))
+      continue;
+
+    if (Configuration::setParam(argv[i])) {
+      i++;
+      continue;
+    }
+
+    if (argv[i][0] == '-') {
+      if (i+1 < argc) {
+        if (Configuration::setParam(&argv[i][1], argv[i+1])) {
+          i += 2;
+          continue;
         }
-        usage(argv[0]);
       }
 
-      strncpy(vncServerName, argv[i], VNCSERVERNAMELEN);
-      vncServerName[VNCSERVERNAMELEN - 1] = '\0';
+      usage(argv[0]);
     }
+
+    strncpy(vncServerName, argv[i], VNCSERVERNAMELEN);
+    vncServerName[VNCSERVERNAMELEN - 1] = '\0';
+    i++;
+  }
 
   mkvnchomedir();
 
