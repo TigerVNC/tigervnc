f9e9fc49835c5f4ff7ac946d24800a7281db52b8
James Li <james_li@skybility.com>
2019-07-09 16:09:39 +0800

    Add watermark for Linux

--- vncviewer/Surface_Win32.cxx
+++ vncviewer/Surface_Win32.cxx
@@ -1,15 +1,15 @@
 /* Copyright 2016 Pierre Ossman for Cendio AB
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
@@ -257,48 +257,3 @@ void Surface::update(const Fl_RGB_Image* image)
   }
 }
 
-covermap Surface::get_cover_map(int X, int Y, int W, int H, int w, int h)
-{
-	covermap map;
-	int alignedX = (X/w)*w;
-	int alignedY = (Y/h)*h;
-	int alignedW = ((X+W-1)/w + 1)*w - alignedX;
-	int alignedH = ((Y+H-1)/h + 1)*h - alignedY;
-	int xSeg = alignedW / w;
-	int ySeg = alignedH / h;
-	int xi;
-	int yi = 0;
-	for(xi = 0; xi < xSeg; xi++){
-		for(yi = 0; yi < ySeg; yi++){
-			int dstX = alignedX + xi * w;
-			int dstY = alignedY + yi * h;
-			int srcX = 0;
-			int srcY = 0;
-			int blockW = w;
-			int blockH = h;
-			int delta = 0;
-			if(dstX < X){
-				delta = X - dstX;
-				srcX += delta;
-				blockW -= delta;
-				dstX = X;
-			}
-			if(dstX - delta + w > X + W){
-				blockW -= (dstX - delta + w - (X + W));
-			}
-			delta = 0;
-			if(dstY < Y){
-				delta = Y - dstY;
-				srcY += delta;
-				blockH -= delta;
-				dstY = Y;
-			}
-			if(dstY -delta + h > Y + H){
-				blockH -= ((dstY - delta + h) - (Y+H));
-			}
-			blockmap block = std::make_tuple(srcX, srcY, dstX, dstY, blockW, blockH);
-			map.push_back(block);
-		}
-	}
-	return map;
-}
--- vncviewer/Surface_X11.cxx
+++ vncviewer/Surface_X11.cxx
@@ -1,15 +1,15 @@
 /* Copyright 2016 Pierre Ossman for Cendio AB
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
@@ -98,12 +98,55 @@ void Surface::blend(int src_x, int src_y, int x, int y, int w, int h, int a)
 
 void Surface::blendWatermark(int X, int Y, int W, int H, int a)
 {
+  Picture winPict, alpha;
+
+  winPict = XRenderCreatePicture(fl_display, fl_window, visFormat, 0, NULL);
+  alpha = alpha_mask(a);
+  covermap map = get_cover_map(X, Y, W, H, w, h);
+  covermap::iterator it;
+  for(it = map.begin(); it != map.end(); it ++){
+      blockmap block = *it;
+      int src_x = std::get<0>(block);
+      int src_y = std::get<1>(block);
+      int dst_x = std::get<2>(block);
+      int dst_y = std::get<3>(block);
+
+      int ow = std::get<4>(block);
+      int oh = std::get<5>(block);
+
+      XRenderComposite(fl_display, PictOpOver, picture, alpha, winPict,
+                       src_x, src_y, 0, 0, dst_x, dst_y, ow, oh);
+  }
+  XRenderFreePicture(fl_display, winPict);
+
+  if (alpha != None)
+    XRenderFreePicture(fl_display, alpha);
 
 }
 
 
 void Surface::blendWatermark(Surface* dst, int X, int Y, int W, int H, int a)
 {
+  Picture alpha;
+
+  alpha = alpha_mask(a);
+  covermap map = get_cover_map(X, Y, W, H, w, h);
+  covermap::iterator it;
+  for(it = map.begin(); it != map.end(); it ++){
+      blockmap block = *it;
+      int src_x = std::get<0>(block);
+      int src_y = std::get<1>(block);
+      int dst_x = std::get<2>(block);
+      int dst_y = std::get<3>(block);
+
+      int ow = std::get<4>(block);
+      int oh = std::get<5>(block);
+
+      XRenderComposite(fl_display, PictOpOver, picture, alpha, dst->picture,
+                       src_x, src_y, 0, 0, dst_x, dst_y, ow, oh);
+  }
+  if (alpha != None)
+    XRenderFreePicture(fl_display, alpha);
 
 }
 
@@ -239,48 +282,3 @@ void Surface::update(const Fl_RGB_Image* image)
   XDestroyImage(img);
 }
 
-covermap Surface::get_cover_map(int X, int Y, int W, int H, int w, int h)
-{
-	covermap map;
-	int alignedX = (X/w)*w;
-	int alignedY = (Y/h)*h;
-	int alignedW = ((X+W-1)/w + 1)*w - alignedX;
-	int alignedH = ((Y+H-1)/h + 1)*h - alignedY;
-	int xSeg = alignedW / w;
-	int ySeg = alignedH / h;
-	int xi;
-	int yi = 0;
-	for(xi = 0; xi < xSeg; xi++){
-		for(yi = 0; yi < ySeg; yi++){
-			int dstX = alignedX + xi * w;
-			int dstY = alignedY + yi * h;
-			int srcX = 0;
-			int srcY = 0;
-			int blockW = w;
-			int blockH = h;
-			int delta = 0;
-			if(dstX < X){
-				delta = X - dstX;
-				srcX += delta;
-				blockW -= delta;
-				dstX = X;
-			}
-			if(dstX - delta + w > X + W){
-				blockW -= (dstX - delta + w - (X + W));
-			}
-			delta = 0;
-			if(dstY < Y){
-				delta = Y - dstY;
-				srcY += delta;
-				blockH -= delta;
-				dstY = Y;
-			}
-			if(dstY -delta + h > Y + H){
-				blockH -= ((dstY - delta + h) - (Y+H));
-			}
-			blockmap block = std::make_tuple(srcX, srcY, dstX, dstY, blockW, blockH);
-			map.push_back(block);
-		}
-	}
-	return map;
-}
