b8a89c7e1e5a95324a6533f276954612e446dee9
Evan Burns <evanburnsdev@gmail.com>
2019-11-08 23:14:38 -0500

    Fixed more permission issues in build.sh

--- .travis/centos8/build.sh
+++ .travis/centos8/build.sh
@@ -25,9 +25,9 @@ chmod a+w ${CURDIR}/builddeps
 docker run --volume ${CURDIR}/rpmbuild:/home/rpm/rpmbuild --volume ${CURDIR}/builddeps:/home/rpm/builddeps --interactive --tty --rm tigervnc/${DOCKER} \
         bash -c "
 	yumdownloader --source libdmx &&
+	rpm -i libdmx-*.rpm
         sudo chown 0.0 ~/rpmbuild/SOURCES/* &&
         sudo chown 0.0 ~/rpmbuild/SPECS/* &&
-	rpm -i libdmx-*.rpm
 	sudo yum-builddep -y ~/rpmbuild/SPECS/libdmx.spec &&
 	rpmbuild -ba ~/rpmbuild/SPECS/libdmx.spec &&
         mv ~/rpmbuild/RPMS/x86_64/libdmx-1.*.rpm ~/builddeps &&
@@ -44,9 +44,9 @@ chmod a+w ${CURDIR}/rpmbuild/{BUILD,BUILDROOT,SRPMS,RPMS,BUILDDEPS}
 docker run --volume ${CURDIR}/rpmbuild:/home/rpm/rpmbuild --volume ${CURDIR}/builddeps:/home/rpm/builddeps --interactive --tty --rm tigervnc/${DOCKER} \
         bash -c "
         yumdownloader --source egl-wayland &&
+        rpm -i egl-wayland-*.rpm
         sudo chown 0.0 ~/rpmbuild/SOURCES/* &&
         sudo chown 0.0 ~/rpmbuild/SPECS/* &&
-        rpm -i egl-wayland-*.rpm
         sudo yum-builddep -y ~/rpmbuild/SPECS/egl-wayland.spec &&
         rpmbuild -ba ~/rpmbuild/SPECS/egl-wayland.spec &&
         mv ~/rpmbuild/RPMS/x86_64/egl-wayland-1.*.rpm ~/builddeps &&
@@ -63,9 +63,9 @@ chmod a+w ${CURDIR}/rpmbuild/{BUILD,BUILDROOT,SRPMS,RPMS,BUILDDEPS}
 docker run --volume ${CURDIR}/rpmbuild:/home/rpm/rpmbuild --volume ${CURDIR}/builddeps:/home/rpm/builddeps --interactive --tty --rm tigervnc/${DOCKER} \
         bash -c "
         yumdownloader --source xorg-x11-server-Xorg &&
+        rpm -i xorg-x11-server-*.rpm
         sudo chown 0.0 ~/rpmbuild/SOURCES/* &&
         sudo chown 0.0 ~/rpmbuild/SPECS/* &&
-        rpm -i xorg-x11-server-*.rpm
         sudo yum-builddep -y ~/rpmbuild/SPECS/xorg-x11-server.spec &&
         rpmbuild -ba ~/rpmbuild/SPECS/xorg-x11-server.spec &&
         mv ~/rpmbuild/RPMS/noarch/xorg-x11-server-source-*.rpm ~/builddeps
