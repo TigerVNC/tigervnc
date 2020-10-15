dac8bba287f19963c3f8bfea07dd8d73801aaa92
KangLin <kl222@126.com>
2020-10-15 15:09:24 +0800

    Support MSVC: add __builtin_ffs

--- win/rfb_win32/DeviceFrameBuffer.cxx
+++ win/rfb_win32/DeviceFrameBuffer.cxx
@@ -34,6 +34,38 @@ using namespace win32;
 
 static LogWriter vlog("DeviceFrameBuffer");
 
+#ifndef __GNUC__
+int __builtin_ffs(unsigned int x)
+{
+	if (x == 0) {
+		return 0;
+	}
+
+	int num = 1;
+	if ((x & 0xffff) == 0) {
+		num += 16;
+		x >>= 16;
+	}
+	if ((x & 0xff) == 0) {
+		num += 8;
+		x >>= 8;
+	}
+	if ((x & 0xf) == 0) {
+		num += 4;
+		x >>= 4;
+	}
+	if ((x & 0x3) == 0) {
+		num += 2;
+		x >>= 2;
+	}
+	if ((x & 0x1) == 0) {
+		num += 1;
+	}
+
+	return num;
+}
+#endif //__GNUC__
+
 BoolParameter DeviceFrameBuffer::useCaptureBlt("UseCaptureBlt",
   "Use a slower capture method that ensures that alpha blended windows appear correctly",
   true);
