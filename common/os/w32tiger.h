03d1591cb86cbb6a1c0ca3f745c90488964542ed
KangLin <kl222@126.com>
2020-11-27 10:23:38 +0800

    Add macro WIN32_LEAN_AND_MEAN

--- common/os/w32tiger.h
+++ common/os/w32tiger.h
@@ -19,7 +19,15 @@
 #ifndef OS_W32TIGER_H
 #define OS_W32TIGER_H
 
+#pragma once
+
 #ifdef WIN32 
+
+// // 包含 SDKDDKVer.h 可定义可用的最高版本的 Windows 平台。
+// 如果希望为之前的 Windows 平台构建应用程序，在包含 SDKDDKVer.h 之前请先包含 WinSDKVer.h 并
+// 将 _WIN32_WINNT 宏设置为想要支持的平台。
+#include <WinSDKVer.h>
+#include <SDKDDKVer.h>
 
  /* Windows has different names for these */
 #define strcasecmp _stricmp
@@ -27,9 +35,18 @@
 
 #include <WinSock2.h>
 #include <ws2tcpip.h>
-#include <Windows.h>
 #include <wininet.h>
 #include <shlobj.h>
+
+#define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
+// Windows 头文件
+#include <windows.h>
+// C 运行时头文件
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
 
 #ifdef __GNUC__
 
