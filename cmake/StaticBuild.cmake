d484001e97ff113e4526bd97661fffcd0372bb34
Pierre Ossman <ossman@cendio.se>
2020-12-07 12:33:50 +0100

    Add comment why unistring is linked dynamically

--- cmake/StaticBuild.cmake
+++ cmake/StaticBuild.cmake
@@ -43,6 +43,8 @@ if(BUILD_STATIC)
 
     set(GETTEXT_LIBRARIES "${GETTEXT_LIBRARIES} -Wl,-Bdynamic")
 
+    # FIXME: MSYS2 doesn't include a static version of this library, so
+    #        we'll have to link it dynamically for now
     if(UNISTRING_LIBRARY)
       set(GETTEXT_LIBRARIES "${GETTEXT_LIBRARIES} -lunistring")
     endif()
