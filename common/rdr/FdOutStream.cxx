736a4dc15cac351da847ca796ac267a28b827d8f
Pierre Ossman <ossman@cendio.se>
2021-03-02 14:36:12 +0100

    Revert "Support TCP_NOPUSH as alternative to TCP_CORK"
    This reverts commit d6e39658ae105fca2cfe37ba736dbf0e8d8356af. Apparently
    this is broken on macOS so it's not something we can make use of.

--- common/rdr/FdOutStream.cxx
+++ common/rdr/FdOutStream.cxx
@@ -74,10 +74,6 @@ void FdOutStream::cork(bool enable)
   int one = enable ? 1 : 0;
   setsockopt(fd, IPPROTO_TCP, TCP_CORK, (char *)&one, sizeof(one));
 #endif
-#ifdef TCP_NOPUSH
-  int one = enable ? 1 : 0;
-  setsockopt(fd, IPPROTO_TCP, TCP_NOPUSH, (char *)&one, sizeof(one));
-#endif
 }
 
 bool FdOutStream::flushBuffer()
