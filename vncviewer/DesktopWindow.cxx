708c997c761b4b683d79ed16dac2288a4f55a5d3
Pierre Ossman <ossman@cendio.se>
2021-12-27 10:38:41 +0100

    TEMP: remove level workaround?

--- vncviewer/DesktopWindow.cxx
+++ vncviewer/DesktopWindow.cxx
@@ -1060,17 +1060,18 @@ void DesktopWindow::fullscreen_on()
   }
 #ifdef __APPLE__
   // This is a workaround for a bug in FLTK, see: https://github.com/fltk/fltk/pull/277
-  int savedLevel = -1;
-  if (shown())
-    savedLevel = cocoa_get_level(this);
+  // FIXME: Does this still happen? Maybe side effect of releasing displays
+  //int savedLevel = -1;
+  //if (shown())
+  //  savedLevel = cocoa_get_level(this);
 #endif
   fullscreen_screens(top, bottom, left, right);
 #ifdef __APPLE__
   // This is a workaround for a bug in FLTK, see: https://github.com/fltk/fltk/pull/277
-  if (savedLevel != -1) {
-    if (cocoa_get_level(this) != savedLevel)
-      cocoa_set_level(this, savedLevel);
-  }
+  //if (savedLevel != -1) {
+  //  if (cocoa_get_level(this) != savedLevel)
+  //    cocoa_set_level(this, savedLevel);
+  //}
 #endif
 
   if (!fullscreen_active())
