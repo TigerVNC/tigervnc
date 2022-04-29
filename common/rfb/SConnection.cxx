ccf759cb513867cdeddedb992a238e1fe89c063d
Samuel Mannehed <samuel@cendio.se>
2022-04-29 10:17:02 +0200

    Initialize SConnection's accessRights
    Properties should be initialized in order to avoid random values in
    case they are used earlier than expected.

--- common/rfb/SConnection.cxx
+++ common/rfb/SConnection.cxx
@@ -59,7 +59,7 @@ SConnection::SConnection()
     is(0), os(0), reader_(0), writer_(0), ssecurity(0),
     authFailureTimer(this, &SConnection::handleAuthFailureTimeout),
     state_(RFBSTATE_UNINITIALISED), preferredEncoding(encodingRaw),
-    clientClipboard(NULL), hasLocalClipboard(false),
+    accessRights(0x0000), clientClipboard(NULL), hasLocalClipboard(false),
     unsolicitedClipboardAttempt(false)
 {
   defaultMajorVersion = 3;
