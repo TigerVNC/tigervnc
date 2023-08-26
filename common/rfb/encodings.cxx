cbafa4283c50fe05171cf5cc9d6e49fe1ee1f659
Pierre Ossman <ossman@cendio.se>
2023-08-26 14:53:12 +0200

    Don't hide OpenH264 encoding constant
    Just because we are aware of the encoding doesn't mean we support it.
    Avoid cluttering everything with #ifdef:s.

--- common/rfb/encodings.cxx
+++ common/rfb/encodings.cxx
@@ -32,9 +32,7 @@ int rfb::encodingNum(const char* name)
   if (strcasecmp(name, "hextile") == 0)  return encodingHextile;
   if (strcasecmp(name, "ZRLE") == 0)     return encodingZRLE;
   if (strcasecmp(name, "Tight") == 0)    return encodingTight;
-#ifdef HAVE_H264
   if (strcasecmp(name, "H.264") == 0)    return encodingH264;
-#endif
   return -1;
 }
 
@@ -48,9 +46,7 @@ const char* rfb::encodingName(int num)
   case encodingHextile:  return "hextile";
   case encodingZRLE:     return "ZRLE";
   case encodingTight:    return "Tight";
-#ifdef HAVE_H264
   case encodingH264:     return "H.264";
-#endif
   default:               return "[unknown encoding]";
   }
 }
