f4cca8e6bdd5c8079b2b7603a7fc96e3c0407808
90 <hi@90.gripe>
2024-03-19 14:03:22 +0000

    Suppress unused param warnings on Windows

--- common/os/os.cxx
+++ common/os/os.cxx
@@ -38,6 +38,7 @@
 #include <windows.h>
 #include <wininet.h> /* MinGW needs it */
 #include <shlobj.h>
+#define UNUSED(x) (void)(x)
 #endif
 
 static const char* getvncdir(bool userDir, const char *xdg_env, const char *xdg_def)
@@ -84,6 +85,8 @@ static const char* getvncdir(bool userDir, const char *xdg_env, const char *xdg_
 
   return dir;
 #else
+  UNUSED(xdg_def);
+  UNUSED(xdg_env);
   if (userDir)
     ret = SHGetSpecialFolderPath(NULL, dir, CSIDL_PROFILE, FALSE);
   else
