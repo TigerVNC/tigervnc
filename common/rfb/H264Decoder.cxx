TigerVNC/tigervnc

commit 12b3f4021641537b90727b23d42de5dff59006cd

Avoid shadowing variables
It's a source of confusion and possibly bugs to reuse the same variable
name for multiple things.

common/rfb/H264Decoder.cxx
@@ -79,9 +79,9 @@ bool H264Decoder::readRect(const Rect& /*r*/,
 
   len = is->readU32();
   os->writeU32(len);
-  uint32_t flags = is->readU32();
+  uint32_t reset = is->readU32();
 
-  os->writeU32(flags);
+  os->writeU32(reset);
 
   if (!is->hasDataOrRestore(len))
     return false;
@@ -100,15 +100,15 @@ void H264Decoder::decodeRect(const Rect& r, const uint8_t* buffer,
 {
   rdr::MemInStream is(buffer, buflen);
   uint32_t len = is.readU32();
-  uint32_t flags = is.readU32();
+  uint32_t reset = is.readU32();
 
   H264DecoderContext* ctx = nullptr;
-  if (flags & resetAllContexts)
+  if (reset & resetAllContexts)
   {
     resetContexts();
     if (!len)
       return;
-    flags &= ~(resetContext | resetAllContexts);
+    reset &= ~(resetContext | resetAllContexts);
   } else {
     ctx = findContext(r);
   }

commit 9abcf0e5e8d02a5536bdc9968b423e73a3e29cce

H264Decoder: Fix context reset
This fixes a regression introduced by
12b3f4021641537b90727b23d42de5dff59006cd.

common/rfb/H264Decoder.cxx
@@ -131,7 +131,7 @@ void H264Decoder::decodeRect(const Rect& r, const uint8_t* buffer,
   if (!ctx->isReady())
     throw Exception("H264Decoder: Context is not ready");
 
-  if (flags & resetContext)
+  if (reset & resetContext)
     ctx->reset();
 
   if (!len)