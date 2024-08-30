49a087e8f8ecafe950a86a71ae6657d6cac8ea77
Pierre Ossman <ossman@cendio.se>
2024-08-30 16:09:38 +0200

    Only read first password from file
    Read just the first password and ignore anything else in the password
    file. This allows you to reuse a password file from the server that also
    includes a view-only password.
    
    This fixes a regression introduced in b99daad.

--- vncviewer/UserDialog.cxx
+++ vncviewer/UserDialog.cxx
@@ -109,7 +109,7 @@ void UserDialog::getUserPasswd(bool secure_, std::string* user,
   }
 
   if (!user && passwordFileName[0]) {
-    std::vector<uint8_t> obfPwd(256);
+    std::vector<uint8_t> obfPwd(8);
     FILE* fp;
 
     fp = fopen(passwordFileName, "rb");
