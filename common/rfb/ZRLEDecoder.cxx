TigerVNC/tigervnc

commit baca73d03217a1c219d9c4f024ffcd39f85fd322

Use templates for optimized code
Avoid preprocessor magic and instead rely on templating to generate
optimized functions for performance critical code.

common/rfb/ZRLEDecoder.cxx
@@ -1,5 +1,5 @@
 /* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
- * Copyright 2009-2017 Pierre Ossman for Cendio AB
+ * Copyright 2009-2022 Pierre Ossman for Cendio AB
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -50,27 +50,22 @@ static inline rdr::U32 readOpaque24B(rdr::InStream* is)
   return r;
 }
 
+template<class T>
+static inline T readPixel(rdr::ZlibInStream* zis)
+{
+  if (sizeof(T) == 1)
+    return zis->readOpaque8();
+  if (sizeof(T) == 2)
+    return zis->readOpaque16();
+  if (sizeof(T) == 4)
+    return zis->readOpaque32();
+}
+
 static inline void zlibHasData(rdr::ZlibInStream* zis, size_t length)
 {
   if (!zis->hasData(length))
     throw Exception("ZRLE decode error");
 }
-
-#define BPP 8
-#include <rfb/zrleDecode.h>
-#undef BPP
-#define BPP 16
-#include <rfb/zrleDecode.h>
-#undef BPP
-#define BPP 32
-#include <rfb/zrleDecode.h>
-#define CPIXEL 24A
-#include <rfb/zrleDecode.h>
-#undef CPIXEL
-#define CPIXEL 24B
-#include <rfb/zrleDecode.h>
-#undef CPIXEL
-#undef BPP
 
 ZRLEDecoder::ZRLEDecoder() : Decoder(DecoderOrdered)
 {
@@ -111,32 +106,185 @@ void ZRLEDecoder::decodeRect(const Rect& r, const void* buffer,
   rdr::MemInStream is(buffer, buflen);
   const rfb::PixelFormat& pf = server.pf();
   switch (pf.bpp) {
-  case 8:  zrleDecode8 (r, &is, &zis, pf, pb); break;
-  case 16: zrleDecode16(r, &is, &zis, pf, pb); break;
-  case 32:
-    {
-      if (pf.depth <= 24) {
-        Pixel maxPixel = pf.pixelFromRGB((rdr::U16)-1, (rdr::U16)-1, (rdr::U16)-1);
-        bool fitsInLS3Bytes = maxPixel < (1<<24);
-        bool fitsInMS3Bytes = (maxPixel & 0xff) == 0;
+  case 8:  zrleDecode<rdr::U8>(r, &is, &zis, pf, pb); break;
+  case 16: zrleDecode<rdr::U16>(r, &is, &zis, pf, pb); break;
+  case 32: zrleDecode<rdr::U32>(r, &is, &zis, pf, pb); break;
+  }
+}
 
-        if ((fitsInLS3Bytes && pf.isLittleEndian()) ||
-            (fitsInMS3Bytes && pf.isBigEndian()))
-        {
-          zrleDecode24A(r, &is, &zis, pf, pb);
-          break;
+template<class T>
+void ZRLEDecoder::zrleDecode(const Rect& r, rdr::InStream* is,
+                             rdr::ZlibInStream* zis,
+                             const PixelFormat& pf,
+                             ModifiablePixelBuffer* pb)
+{
+  int length = is->readU32();
+  zis->setUnderlying(is, length);
+  Rect t;
+  T buf[64 * 64];
+
+  Pixel maxPixel = pf.pixelFromRGB((rdr::U16)-1, (rdr::U16)-1, (rdr::U16)-1);
+  bool fitsInLS3Bytes = maxPixel < (1<<24);
+  bool fitsInMS3Bytes = (maxPixel & 0xff) == 0;
+  bool isLowCPixel = (sizeof(T) == 4) &&
+                     ((fitsInLS3Bytes && pf.isLittleEndian()) ||
+                      (fitsInMS3Bytes && pf.isBigEndian()));
+  bool isHighCPixel = (sizeof(T) == 4) &&
+                      ((fitsInLS3Bytes && pf.isBigEndian()) ||
+                       (fitsInMS3Bytes && pf.isLittleEndian()));
+
+  for (t.tl.y = r.tl.y; t.tl.y < r.br.y; t.tl.y += 64) {
+
+    t.br.y = __rfbmin(r.br.y, t.tl.y + 64);
+
+    for (t.tl.x = r.tl.x; t.tl.x < r.br.x; t.tl.x += 64) {
+
+      t.br.x = __rfbmin(r.br.x, t.tl.x + 64);
+
+      zlibHasData(zis, 1);
+      int mode = zis->readU8();
+      bool rle = mode & 128;
+      int palSize = mode & 127;
+      T palette[128];
+
+      if (isLowCPixel || isHighCPixel)
+        zlibHasData(zis, 3 * palSize);
+      else
+        zlibHasData(zis, sizeof(T) * palSize);
+
+      for (int i = 0; i < palSize; i++) {
+        if (isLowCPixel)
+          palette[i] = readOpaque24A(zis);
+        else if (isHighCPixel)
+          palette[i] = readOpaque24B(zis);
+        else
+          palette[i] = readPixel<T>(zis);
+      }
+
+      if (palSize == 1) {
+        T pix = palette[0];
+        pb->fillRect(pf, t, &pix);
+        continue;
+      }
+
+      if (!rle) {
+        if (palSize == 0) {
+
+          // raw
+
+          if (isLowCPixel || isHighCPixel)
+            zlibHasData(zis, 3 * t.area());
+          else
+            zlibHasData(zis, sizeof(T) * t.area());
+
+          if (isLowCPixel || isHighCPixel) {
+            for (T* ptr = buf; ptr < buf+t.area(); ptr++) {
+              if (isLowCPixel)
+                *ptr = readOpaque24A(zis);
+              else
+                *ptr = readOpaque24B(zis);
+            }
+          } else {
+            zis->readBytes(buf, t.area() * sizeof(T));
+          }
+
+        } else {
+
+          // packed pixels
+          int bppp = ((palSize > 16) ? 8 :
+                      ((palSize > 4) ? 4 : ((palSize > 2) ? 2 : 1)));
+
+          T* ptr = buf;
+
+          for (int i = 0; i < t.height(); i++) {
+            T* eol = ptr + t.width();
+            rdr::U8 byte = 0;
+            rdr::U8 nbits = 0;
+
+            while (ptr < eol) {
+              if (nbits == 0) {
+                zlibHasData(zis, 1);
+                byte = zis->readU8();
+                nbits = 8;
+              }
+              nbits -= bppp;
+              rdr::U8 index = (byte >> nbits) & ((1 << bppp) - 1) & 127;
+              *ptr++ = palette[index];
+            }
+          }
         }
 
-        if ((fitsInLS3Bytes && pf.isBigEndian()) ||
-            (fitsInMS3Bytes && pf.isLittleEndian()))
-        {
-          zrleDecode24B(r, &is, &zis, pf, pb);
-          break;
+      } else {
+
+        if (palSize == 0) {
+
+          // plain RLE
+
+          T* ptr = buf;
+          T* end = ptr + t.area();
+          while (ptr < end) {
+            T pix;
+            if (isLowCPixel || isHighCPixel)
+              zlibHasData(zis, 3);
+            else
+              zlibHasData(zis, sizeof(T));
+            if (isLowCPixel)
+              pix = readOpaque24A(zis);
+            else if (isHighCPixel)
+              pix = readOpaque24B(zis);
+            else
+              pix = readPixel<T>(zis);
+            int len = 1;
+            int b;
+            do {
+              zlibHasData(zis, 1);
+              b = zis->readU8();
+              len += b;
+            } while (b == 255);
+
+            if (end - ptr < len) {
+              throw Exception ("ZRLE decode error");
+            }
+
+            while (len-- > 0) *ptr++ = pix;
+
+          }
+        } else {
+
+          // palette RLE
+
+          T* ptr = buf;
+          T* end = ptr + t.area();
+          while (ptr < end) {
+            zlibHasData(zis, 1);
+            int index = zis->readU8();
+            int len = 1;
+            if (index & 128) {
+              int b;
+              do {
+                zlibHasData(zis, 1);
+                b = zis->readU8();
+                len += b;
+              } while (b == 255);
+
+              if (end - ptr < len) {
+                throw Exception ("ZRLE decode error");
+              }
+            }
+
+            index &= 127;
+
+            T pix = palette[index];
+
+            while (len-- > 0) *ptr++ = pix;
+          }
         }
       }
 
-      zrleDecode32(r, &is, &zis, pf, pb);
-      break;
+      pb->imageRect(pf, t, buf);
     }
   }
+
+  zis->flushUnderlying();
+  zis->setUnderlying(NULL, 0);
 }

common/rfb/EncodeManager.cxx
@@ -1,6 +1,6 @@
 /* Copyright (C) 2000-2003 Constantin Kaplinsky.  All Rights Reserved.
  * Copyright (C) 2011 D. R. Commander.  All Rights Reserved.
- * Copyright 2014-2018 Pierre Ossman for Cendio AB
+ * Copyright 2014-2022 Pierre Ossman for Cendio AB
  * Copyright 2018 Peter Astrand for Cendio AB
  * 
  * This is free software; you can redistribute it and/or modify
@@ -1065,14 +1065,78 @@ rdr::U8* EncodeManager::OffsetPixelBuffer::getBufferRW(const Rect& /*r*/, int* /
   throw rfb::Exception("Invalid write attempt to OffsetPixelBuffer");
 }
 
-// Preprocessor generated, optimised methods
+template<class T>
+inline bool EncodeManager::checkSolidTile(const Rect& r,
+                                          const T colourValue,
+                                          const PixelBuffer *pb)
+{
+  int w, h;
+  const T* buffer;
+  int stride, pad;
 
-#define BPP 8
-#include "EncodeManagerBPP.cxx"
-#undef BPP
-#define BPP 16
-#include "EncodeManagerBPP.cxx"
-#undef BPP
-#define BPP 32
-#include "EncodeManagerBPP.cxx"
-#undef BPP
+  w = r.width();
+  h = r.height();
+
+  buffer = (const T*)pb->getBuffer(r, &stride);
+  pad = stride - w;
+
+  while (h--) {
+    int w_ = w;
+    while (w_--) {
+      if (*buffer != colourValue)
+        return false;
+      buffer++;
+    }
+    buffer += pad;
+  }
+
+  return true;
+}
+
+template<class T>
+inline bool EncodeManager::analyseRect(int width, int height,
+                                       const T* buffer, int stride,
+                                       struct RectInfo *info, int maxColours)
+{
+  int pad;
+
+  T colour;
+  int count;
+
+  info->rleRuns = 0;
+  info->palette.clear();
+
+  pad = stride - width;
+
+  // For efficiency, we only update the palette on changes in colour
+  colour = buffer[0];
+  count = 0;
+  while (height--) {
+    int w_ = width;
+    while (w_--) {
+      if (*buffer != colour) {
+        if (!info->palette.insert(colour, count))
+          return false;
+        if (info->palette.size() > maxColours)
+          return false;
+
+        // FIXME: This doesn't account for switching lines
+        info->rleRuns++;
+
+        colour = *buffer;
+        count = 0;
+      }
+      buffer++;
+      count++;
+    }
+    buffer += pad;
+  }
+
+  // Make sure the final pixels also get counted
+  if (!info->palette.insert(colour, count))
+    return false;
+  if (info->palette.size() > maxColours)
+    return false;
+
+  return true;
+}

common/rfb/EncodeManager.h
@@ -1,6 +1,6 @@
 /* Copyright (C) 2000-2003 Constantin Kaplinsky.  All Rights Reserved.
  * Copyright (C) 2011 D. R. Commander.  All Rights Reserved.
- * Copyright 2014-2018 Pierre Ossman for Cendio AB
+ * Copyright 2014-2022 Pierre Ossman for Cendio AB
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -97,22 +97,13 @@ namespace rfb {
                      struct RectInfo *info, int maxColours);
 
   protected:
-    // Preprocessor generated, optimised methods
-    inline bool checkSolidTile(const Rect& r, rdr::U8 colourValue,
+    // Templated, optimised methods
+    template<class T>
+    inline bool checkSolidTile(const Rect& r, const T,
                                const PixelBuffer *pb);
-    inline bool checkSolidTile(const Rect& r, rdr::U16 colourValue,
-                               const PixelBuffer *pb);
-    inline bool checkSolidTile(const Rect& r, rdr::U32 colourValue,
-                               const PixelBuffer *pb);
-
+    template<class T>
     inline bool analyseRect(int width, int height,
-                            const rdr::U8* buffer, int stride,
-                            struct RectInfo *info, int maxColours);
-    inline bool analyseRect(int width, int height,
-                            const rdr::U16* buffer, int stride,
-                            struct RectInfo *info, int maxColours);
-    inline bool analyseRect(int width, int height,
-                            const rdr::U32* buffer, int stride,
+                            const T* buffer, int stride,
                             struct RectInfo *info, int maxColours);
 
   protected:

common/rfb/EncodeManagerBPP.cxx
@@ -1,98 +1,0 @@
-/* Copyright (C) 2000-2003 Constantin Kaplinsky.  All Rights Reserved.
- * Copyright (C) 2011 D. R. Commander.  All Rights Reserved.
- * Copyright 2014 Pierre Ossman for Cendio AB
- * 
- * This is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this software; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
- * USA.
- */
-
-#define CONCAT2(a,b) a##b
-#define CONCAT2E(a,b) CONCAT2(a,b)
-
-#define UBPP CONCAT2E(U,BPP)
-
-inline bool EncodeManager::checkSolidTile(const Rect& r,
-                                          rdr::UBPP colourValue,
-                                          const PixelBuffer *pb)
-{
-  int w, h;
-  const rdr::UBPP* buffer;
-  int stride, pad;
-
-  w = r.width();
-  h = r.height();
-
-  buffer = (const rdr::UBPP*)pb->getBuffer(r, &stride);
-  pad = stride - w;
-
-  while (h--) {
-    int w_ = w;
-    while (w_--) {
-      if (*buffer != colourValue)
-        return false;
-      buffer++;
-    }
-    buffer += pad;
-  }
-
-  return true;
-}
-
-inline bool EncodeManager::analyseRect(int width, int height,
-                                       const rdr::UBPP* buffer, int stride,
-                                       struct RectInfo *info, int maxColours)
-{
-  int pad;
-
-  rdr::UBPP colour;
-  int count;
-
-  info->rleRuns = 0;
-  info->palette.clear();
-
-  pad = stride - width;
-
-  // For efficiency, we only update the palette on changes in colour
-  colour = buffer[0];
-  count = 0;
-  while (height--) {
-    int w_ = width;
-    while (w_--) {
-      if (*buffer != colour) {
-        if (!info->palette.insert(colour, count))
-          return false;
-        if (info->palette.size() > maxColours)
-          return false;
-
-        // FIXME: This doesn't account for switching lines
-        info->rleRuns++;
-
-        colour = *buffer;
-        count = 0;
-      }
-      buffer++;
-      count++;
-    }
-    buffer += pad;
-  }
-
-  // Make sure the final pixels also get counted
-  if (!info->palette.insert(colour, count))
-    return false;
-  if (info->palette.size() > maxColours)
-    return false;
-
-  return true;
-}

common/rfb/HextileDecoder.cxx
@@ -1,4 +1,5 @@
 /* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
+ * Copyright 2014-2022 Pierre Ossman for Cendio AB
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,21 +25,13 @@
 #include <rdr/MemInStream.h>
 #include <rdr/OutStream.h>
 
+#include <rfb/Exception.h>
 #include <rfb/ServerParams.h>
 #include <rfb/PixelBuffer.h>
 #include <rfb/HextileDecoder.h>
+#include <rfb/hextileConstants.h>
 
 using namespace rfb;
-
-#define BPP 8
-#include <rfb/hextileDecode.h>
-#undef BPP
-#define BPP 16
-#include <rfb/hextileDecode.h>
-#undef BPP
-#define BPP 32
-#include <rfb/hextileDecode.h>
-#undef BPP
 
 HextileDecoder::HextileDecoder() : Decoder(DecoderPlain)
 {
@@ -127,8 +120,87 @@ void HextileDecoder::decodeRect(const Rect& r, const void* buffer,
   rdr::MemInStream is(buffer, buflen);
   const PixelFormat& pf = server.pf();
   switch (pf.bpp) {
-  case 8:  hextileDecode8 (r, &is, pf, pb); break;
-  case 16: hextileDecode16(r, &is, pf, pb); break;
-  case 32: hextileDecode32(r, &is, pf, pb); break;
+  case 8:  hextileDecode<rdr::U8 >(r, &is, pf, pb); break;
+  case 16: hextileDecode<rdr::U16>(r, &is, pf, pb); break;
+  case 32: hextileDecode<rdr::U32>(r, &is, pf, pb); break;
+  }
+}
+
+template<class T>
+inline T HextileDecoder::readPixel(rdr::InStream* is)
+{
+  if (sizeof(T) == 1)
+    return is->readOpaque8();
+  if (sizeof(T) == 2)
+    return is->readOpaque16();
+  if (sizeof(T) == 4)
+    return is->readOpaque32();
+}
+
+template<class T>
+void HextileDecoder::hextileDecode(const Rect& r, rdr::InStream* is,
+                                   const PixelFormat& pf,
+                                   ModifiablePixelBuffer* pb)
+{
+  Rect t;
+  T bg = 0;
+  T fg = 0;
+  T buf[16 * 16];
+
+  for (t.tl.y = r.tl.y; t.tl.y < r.br.y; t.tl.y += 16) {
+
+    t.br.y = __rfbmin(r.br.y, t.tl.y + 16);
+
+    for (t.tl.x = r.tl.x; t.tl.x < r.br.x; t.tl.x += 16) {
+
+      t.br.x = __rfbmin(r.br.x, t.tl.x + 16);
+
+      int tileType = is->readU8();
+
+      if (tileType & hextileRaw) {
+        is->readBytes(buf, t.area() * sizeof(T));
+        pb->imageRect(pf, t, buf);
+        continue;
+      }
+
+      if (tileType & hextileBgSpecified)
+        bg = readPixel<T>(is);
+
+      int len = t.area();
+      T* ptr = buf;
+      while (len-- > 0) *ptr++ = bg;
+
+      if (tileType & hextileFgSpecified)
+        fg = readPixel<T>(is);
+
+      if (tileType & hextileAnySubrects) {
+        int nSubrects = is->readU8();
+
+        for (int i = 0; i < nSubrects; i++) {
+
+          if (tileType & hextileSubrectsColoured)
+            fg = readPixel<T>(is);
+
+          int xy = is->readU8();
+          int wh = is->readU8();
+
+          int x = ((xy >> 4) & 15);
+          int y = (xy & 15);
+          int w = ((wh >> 4) & 15) + 1;
+          int h = (wh & 15) + 1;
+          if (x + w > 16 || y + h > 16) {
+            throw rfb::Exception("HEXTILE_DECODE: Hextile out of bounds");
+          }
+          T* ptr = buf + y * t.width() + x;
+          int rowAdd = t.width() - w;
+          while (h-- > 0) {
+            int len = w;
+            while (len-- > 0) *ptr++ = fg;
+            ptr += rowAdd;
+          }
+        }
+      }
+      pb->imageRect(pf, t, buf);
+    }
   }
 }

common/rfb/HextileDecoder.h
@@ -1,4 +1,5 @@
 /* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
+ * Copyright 2014-2022 Pierre Ossman for Cendio AB
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,6 +23,8 @@
 
 namespace rfb {
 
+  class PixelFormat;
+
   class HextileDecoder : public Decoder {
   public:
     HextileDecoder();
@@ -31,6 +34,13 @@ namespace rfb {
     virtual void decodeRect(const Rect& r, const void* buffer,
                             size_t buflen, const ServerParams& server,
                             ModifiablePixelBuffer* pb);
+  private:
+    template<class T>
+    inline T readPixel(rdr::InStream* is);
+    template<class T>
+    void hextileDecode(const Rect& r, rdr::InStream* is,
+                       const PixelFormat& pf,
+                       ModifiablePixelBuffer* pb);
   };
 }
 #endif

commit 6d9017eeb364491cf2acdf1c7e61aee8dd198527

Correctly handle ZRLE cursors
Cursor data has a depth of 32 bits and hence cannot use CPIXELs.

This is a regression from baca73d.

common/rfb/ZRLEDecoder.cxx
@@ -125,10 +125,10 @@ void ZRLEDecoder::zrleDecode(const Rect& r, rdr::InStream* is,
   Pixel maxPixel = pf.pixelFromRGB((uint16_t)-1, (uint16_t)-1, (uint16_t)-1);
   bool fitsInLS3Bytes = maxPixel < (1<<24);
   bool fitsInMS3Bytes = (maxPixel & 0xff) == 0;
-  bool isLowCPixel = (sizeof(T) == 4) &&
+  bool isLowCPixel = (sizeof(T) == 4) && (pf.depth <= 24) &&
                      ((fitsInLS3Bytes && pf.isLittleEndian()) ||
                       (fitsInMS3Bytes && pf.isBigEndian()));
-  bool isHighCPixel = (sizeof(T) == 4) &&
+  bool isHighCPixel = (sizeof(T) == 4) && (pf.depth <= 24) &&
                       ((fitsInLS3Bytes && pf.isBigEndian()) ||
                        (fitsInMS3Bytes && pf.isLittleEndian()));
 