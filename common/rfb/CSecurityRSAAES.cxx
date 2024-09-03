455ad1ce5db4a1d47200ce0eea3584785007e28a
Pierre Ossman <ossman@cendio.se>
2024-09-03 08:03:41 +0200

    Correctly handling rejecting server key
    This is the user actively rejecting the connection, and should be
    signalled as such for correct behaviour.

--- common/rfb/CSecurityRSAAES.cxx
+++ common/rfb/CSecurityRSAAES.cxx
@@ -215,7 +215,7 @@ void CSecurityRSAAES::verifyServer()
     "Please verify that the information is correct and press \"Yes\". "
     "Otherwise press \"No\"", f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7]);
   if (!cc->showMsgBox(MsgBoxFlags::M_YESNO, title, text.c_str()))
-    throw Exception("server key mismatch");
+    throw AuthCancelledException();
 }
 
 void CSecurityRSAAES::writeRandom()
