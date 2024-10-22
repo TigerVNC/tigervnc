1b4af5c586eb7a30a38c82fd088c1fa47a83e72f
Linn Mattsson <linma@cendio.se>
2024-10-22 15:45:27 +0200

    TigerVNC 1.14.1

--- unix/xserver/hw/vnc/xvnc.c
+++ unix/xserver/hw/vnc/xvnc.c
@@ -73,7 +73,7 @@ extern char buildtime[];
 
 #include "version-config.h"
 
-#define XVNCVERSION "TigerVNC 1.14.0"
+#define XVNCVERSION "TigerVNC 1.14.1"
 #define XVNCCOPYRIGHT ("Copyright (C) 1999-2024 TigerVNC Team and many others (see README.rst)\n" \
                        "See https://www.tigervnc.org for information on TigerVNC.\n")
 
