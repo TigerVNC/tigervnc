7e7bdf6cd35ca1f9ed75219b6220d010f6c66bfa
Pierre Ossman <ossman@cendio.se>
2024-12-08 00:24:30 +0100

    Show error grabbing keyboard
    It is now very likely that this happens as we will not have permission
    to do this by default on macOS. So present a message to the user so they
    understand why the keyboard isn't working as expected.

--- vncviewer/DesktopWindow.cxx
+++ vncviewer/DesktopWindow.cxx
@@ -239,7 +239,7 @@ DesktopWindow::DesktopWindow(int w, int h,
 
   combo = HotKeyHandler::comboPrefix(hotKeyCombo);
   if (combo[0] != '\0')
-    addOverlay(_("Press %sM to open the context menu"), combo);
+    addOverlayTip(_("Press %sM to open the context menu"), combo);
 
   // By default we get a slight delay when we warp the pointer, something
   // we don't want or we'll get jerky movement
@@ -721,13 +721,36 @@ void DesktopWindow::resize(int x, int y, int w, int h)
   }
 }
 
-void DesktopWindow::addOverlay(const char* text, ...)
+void DesktopWindow::addOverlayTip(const char* text, ...)
+{
+  va_list ap;
+  char textbuf[1024];
+
+  va_start(ap, text);
+  vsnprintf(textbuf, sizeof(textbuf), text, ap);
+  textbuf[sizeof(textbuf)-1] = '\0';
+  va_end(ap);
+
+  addOverlay(textbuf, false);
+}
+
+void DesktopWindow::addOverlayError(const char* text, ...)
+{
+  va_list ap;
+  char textbuf[1024];
+
+  va_start(ap, text);
+  vsnprintf(textbuf, sizeof(textbuf), text, ap);
+  textbuf[sizeof(textbuf)-1] = '\0';
+  va_end(ap);
+
+  addOverlay(textbuf, true);
+}
+
+void DesktopWindow::addOverlay(const char *text, bool always)
 {
   const Fl_Fontsize fontsize = 16;
   const int margin = 10;
-
-  va_list ap;
-  char textbuf[1024];
 
   std::map<std::string, time_t>::iterator iter;
 
@@ -746,11 +769,6 @@ void DesktopWindow::addOverlay(const char* text, ...)
 
   struct Overlay overlay;
 
-  va_start(ap, text);
-  vsnprintf(textbuf, sizeof(textbuf), text, ap);
-  textbuf[sizeof(textbuf)-1] = '\0';
-  va_end(ap);
-
   // Purge all old entries
   for (iter = overlayTimes.begin(); iter != overlayTimes.end(); ) {
     if ((time(nullptr) - iter->second) >= OVERLAY_REPEAT_TIMEOUT)
@@ -759,11 +777,13 @@ void DesktopWindow::addOverlay(const char* text, ...)
       iter++;
   }
 
-  // Recently shown?
-  if (overlayTimes.count(textbuf) > 0)
-    return;
+  if (!always) {
+    // Recently shown?
+    if (overlayTimes.count(text) > 0)
+      return;
 
-  overlayTimes[textbuf] = time(nullptr);
+    overlayTimes[text] = time(nullptr);
+  }
 
 #if !defined(WIN32) && !defined(__APPLE__)
   // FLTK < 1.3.5 crashes if fl_gc is unset
@@ -773,7 +793,7 @@ void DesktopWindow::addOverlay(const char* text, ...)
 
   fl_font(FL_HELVETICA, fontsize);
   w = 0;
-  fl_measure(textbuf, w, h);
+  fl_measure(text, w, h);
 
   // Margins
   w += margin * 2 * 2;
@@ -786,7 +806,7 @@ void DesktopWindow::addOverlay(const char* text, ...)
 
   fl_font(FL_HELVETICA, fontsize);
   fl_color(FL_WHITE);
-  fl_draw(textbuf, 0, 0, w, h, FL_ALIGN_CENTER);
+  fl_draw(text, 0, 0, w, h, FL_ALIGN_CENTER);
 
   imageText = surface->image();
   delete surface;
@@ -888,7 +908,7 @@ int DesktopWindow::handle(int event)
 
       combo = HotKeyHandler::comboPrefix(hotKeyCombo);
       if (combo[0] != '\0')
-        addOverlay(_("Press %sEnter to leave full-screen mode"), combo);
+        addOverlayTip(_("Press %sEnter to leave full-screen mode"), combo);
     }
 
 #ifdef __APPLE__
@@ -1163,6 +1183,7 @@ void DesktopWindow::grabKeyboard()
   ret = win32_enable_lowlevel_keyboard(fl_xid(this));
   if (ret != 0) {
     vlog.error(_("Failure grabbing keyboard"));
+    addOverlayError(_("Failure grabbing keyboard"));
     return;
   }
 #elif defined(__APPLE__)
@@ -1171,6 +1192,7 @@ void DesktopWindow::grabKeyboard()
   ret = cocoa_tap_keyboard();
   if (!ret) {
     vlog.error(_("Failure grabbing keyboard"));
+    addOverlayError(_("Failure grabbing keyboard"));
     return;
   }
 #else
@@ -1196,6 +1218,7 @@ void DesktopWindow::grabKeyboard()
 
     if (ret) {
       vlog.error(_("Failure grabbing keyboard"));
+      addOverlayError(_("Failure grabbing keyboard"));
       return;
     }
   }
@@ -1210,7 +1233,7 @@ void DesktopWindow::grabKeyboard()
 
   combo = (char*)HotKeyHandler::comboPrefix(hotKeyCombo, true);
   if (combo[0] != '\0')
-    addOverlay(_("Press %s to release control from the session"), combo);
+    addOverlayTip(_("Press %s to release control from the session"), combo);
 }
 
 
