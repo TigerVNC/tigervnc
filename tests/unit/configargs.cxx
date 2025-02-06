TigerVNC/tigervnc

commit e97e225024fbf91a0f5dc78629e09fc9e56e3ba1

Consolidate argument parsing in single function
Avoid duplicating this complexity everywhere and make sure all commands
get the same handling and fixes.

common/rfb/Configuration.cxx
@@ -95,31 +95,6 @@ bool Configuration::set(const char* paramName, int len,
   return _next ? _next->set(paramName, len, val, immutable) : false;
 }
 
-bool Configuration::set(const char* config, bool immutable) {
-  bool hyphen = false;
-  if (config[0] == '-') {
-    hyphen = true;
-    config++;
-    if (config[0] == '-') config++; // allow gnu-style --<option>
-  }
-  const char* equal = strchr(config, '=');
-  if (equal) {
-    return set(config, equal-config, equal+1, immutable);
-  } else if (hyphen) {
-    VoidParameter* current = head;
-    while (current) {
-      if (strcasecmp(current->getName(), config) == 0) {
-        bool b = current->setParam();
-        if (b && immutable) 
-	  current->setImmutable();
-        return b;
-      }
-      current = current->_next;
-    }
-  }    
-  return _next ? _next->set(config, immutable) : false;
-}
-
 VoidParameter* Configuration::get(const char* param)
 {
   VoidParameter* current = head;
@@ -187,6 +162,70 @@ bool Configuration::remove(const char* param) {
   }
 
   return false;
+}
+
+int Configuration::handleArg(int argc, char* argv[], int index)
+{
+  std::string param, val;
+  const char* equal = strchr(argv[index], '=');
+
+  if (equal == argv[index])
+    return 0;
+
+  if (equal) {
+    param.assign(argv[index], equal-argv[index]);
+    val.assign(equal+1);
+  } else {
+    param.assign(argv[index]);
+  }
+
+  if ((param.length() > 0) && (param[0] == '-')) {
+    // allow gnu-style --<option>
+    if ((param.length() > 1) && (param[1] == '-'))
+      param = param.substr(2);
+    else
+      param = param.substr(1);
+  } else {
+    // All command line arguments need either an initial '-', or an '='
+    if (val.empty())
+      return 0;
+  }
+
+  if (!val.empty())
+    return set(param.c_str(), val.c_str()) ? 1 : 0;
+
+  VoidParameter* current = head;
+  while (current) {
+    if (strcasecmp(current->getName(), param.c_str()) != 0) {
+      current = current->_next;
+      continue;
+    }
+
+    // We need to resolve an ambiguity for booleans
+    if (dynamic_cast<BoolParameter*>(current) != nullptr) {
+      if (index+1 < argc) {
+        // FIXME: Should not duplicate the list of values here
+        if ((strcasecmp(argv[index+1], "0") == 0) ||
+            (strcasecmp(argv[index+1], "1") == 0) ||
+            (strcasecmp(argv[index+1], "true") == 0) ||
+            (strcasecmp(argv[index+1], "false") == 0) ||
+            (strcasecmp(argv[index+1], "yes") == 0) ||
+            (strcasecmp(argv[index+1], "no") == 0)) {
+            return current->setParam(argv[index+1]) ? 2 : 0;
+        }
+      }
+    }
+
+    if (current->setParam())
+      return 1;
+
+    if (index+1 >= argc)
+      return 0;
+
+    return current->setParam(argv[index+1]) ? 2 : 0;
+  }
+
+  return _next ? _next->handleArg(argc, argv, index) : 0;
 }
 
 

tests/unit/configargs.cxx
@@ -1,0 +1,326 @@
+/* Copyright 2025 Pierre Ossman <ossman@cendio.se> for Cendio AB
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+ * USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <string.h>
+
+#include <rfb/Configuration.h>
+
+static rfb::BoolParameter boolparam("boolparam", "", false);
+static rfb::IntParameter intparam("intparam", "", 0);
+static rfb::StringParameter strparam("strparam", "", "");
+
+#define ASSERT_EQ_I(expr, val) if ((expr) != (val)) { \
+  printf("FAILED on line %d (%s equals %d, expected %d)\n", __LINE__, #expr, (int)(expr), (int)(val)); \
+  return; \
+}
+
+#define ASSERT_EQ_S(expr, val) if (strcmp((expr), (val)) != 0) { \
+  printf("FAILED on line %d (%s equals %s, expected %s)\n", __LINE__, #expr, (const char*)(expr), (const char*)(val)); \
+  return; \
+}
+
+static void test_args()
+{
+  int ret;
+  std::vector<const char*> argv;
+
+  printf("%s: ", __func__);
+
+  boolparam.setParam(true);
+  intparam.setParam(1);
+  strparam.setParam("test");
+
+  // Just program name
+  argv = {"prog" };
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 0);
+  ASSERT_EQ_I(ret, 0);
+  ASSERT_EQ_I(boolparam, true);
+  ASSERT_EQ_I(intparam, 1);
+  ASSERT_EQ_S(strparam, "test");
+
+  // A bunch of standard arguments
+  argv = {"prog", "arg1", "arg2", "arg3" };
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 2);
+  ASSERT_EQ_I(ret, 0);
+  ASSERT_EQ_I(boolparam, true);
+  ASSERT_EQ_I(intparam, 1);
+  ASSERT_EQ_S(strparam, "test");
+
+  // A parameter without any dash
+  argv = {"prog", "strparam", "intparam" };
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 0);
+  ASSERT_EQ_I(boolparam, true);
+  ASSERT_EQ_I(intparam, 1);
+  ASSERT_EQ_S(strparam, "test");
+
+  printf("OK\n");
+}
+
+static void test_none()
+{
+  int ret;
+  std::vector<const char*> argv;
+
+  printf("%s: ", __func__);
+
+  boolparam.setParam(true);
+  intparam.setParam(1);
+  strparam.setParam("test");
+
+  // int argument
+  argv = {"prog", "intparam=12", "34"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_I(intparam, 12);
+
+  // string argument
+  argv = {"prog", "strparam=foo", "bar" };
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_S(strparam, "foo");
+
+  // Bad parameter
+  argv = {"prog", "fooparam=123"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 0);
+
+  printf("OK\n");
+}
+
+static void test_single()
+{
+  int ret;
+  std::vector<const char*> argv;
+
+  printf("%s: ", __func__);
+
+  boolparam.setParam(true);
+  intparam.setParam(1);
+  strparam.setParam("test");
+
+  // int argument
+  argv = {"prog", "-intparam", "12", "34"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 2);
+  ASSERT_EQ_I(intparam, 12);
+
+  // int argument with equals
+  argv = {"prog", "-intparam=12", "34"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_I(intparam, 12);
+
+  // string argument
+  argv = {"prog", "-strparam", "foo", "bar" };
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 2);
+  ASSERT_EQ_S(strparam, "foo");
+
+  // string argument with equals
+  argv = {"prog", "-strparam=foo", "bar" };
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_S(strparam, "foo");
+
+  // Missing argument
+  intparam.setParam(1);
+  argv = {"prog", "-intparam"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 0);
+  ASSERT_EQ_I(intparam, 1);
+
+  // Bad parameter
+  argv = {"prog", "-fooparam", "123"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 0);
+
+  // Bad parameter with equals
+  argv = {"prog", "-fooparam=123"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 0);
+
+  printf("OK\n");
+}
+
+static void test_double()
+{
+  int ret;
+  std::vector<const char*> argv;
+
+  printf("%s: ", __func__);
+
+  boolparam.setParam(true);
+  intparam.setParam(1);
+  strparam.setParam("test");
+
+  // int argument
+  argv = {"prog", "--intparam", "12", "34"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 2);
+  ASSERT_EQ_I(intparam, 12);
+
+  // int argument with equals
+  argv = {"prog", "--intparam=12", "34"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_I(intparam, 12);
+
+  // string argument
+  argv = {"prog", "--strparam", "foo", "bar" };
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 2);
+  ASSERT_EQ_S(strparam, "foo");
+
+  // string argument with equals
+  argv = {"prog", "--strparam=foo", "bar" };
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_S(strparam, "foo");
+
+  // Missing argument
+  intparam.setParam(1);
+  argv = {"prog", "--intparam"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 0);
+  ASSERT_EQ_I(intparam, 1);
+
+  // Bad parameter
+  argv = {"prog", "--fooparam", "123"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 0);
+
+  // Bad parameter with equals
+  argv = {"prog", "--fooparam=123"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 0);
+
+  printf("OK\n");
+}
+
+static void test_bool()
+{
+  int ret;
+  std::vector<const char*> argv;
+
+  printf("%s: ", __func__);
+
+  // solo bool (single)
+  boolparam.setParam(false);
+  argv = {"prog", "-boolparam"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_I(boolparam, true);
+
+  // solo bool (double)
+  boolparam.setParam(false);
+  argv = {"prog", "--boolparam"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_I(boolparam, true);
+
+  // bool argument (single)
+  boolparam.setParam(true);
+  argv = {"prog", "-boolparam", "false", "true"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 2);
+  ASSERT_EQ_I(boolparam, false);
+
+  // bool argument (double)
+  boolparam.setParam(true);
+  argv = {"prog", "--boolparam", "false", "true"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 2);
+  ASSERT_EQ_I(boolparam, false);
+
+  // bool argument equals (single)
+  boolparam.setParam(true);
+  argv = {"prog", "-boolparam=false", "true"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_I(boolparam, false);
+
+  // bool argument equals (double)
+  boolparam.setParam(true);
+  argv = {"prog", "--boolparam=false", "true"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_I(boolparam, false);
+
+  // bool bad argument (single)
+  boolparam.setParam(false);
+  argv = {"prog", "-boolparam", "foo", "false"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_I(boolparam, true);
+
+  // bool bad argument (double)
+  boolparam.setParam(false);
+  argv = {"prog", "--boolparam", "foo", "false"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_I(boolparam, true);
+
+  printf("OK\n");
+}
+
+int main(int /*argc*/, char** /*argv*/)
+{
+  printf("Configuration argument handling test\n");
+
+  test_args();
+  test_none();
+  test_single();
+  test_double();
+  test_bool();
+
+  return 0;
+}

common/rfb/Configuration.h
@@ -73,9 +73,6 @@ namespace rfb {
     // - Set named parameter to value
     bool set(const char* param, const char* value, bool immutable=false);
 
-    // - Set parameter to value (separated by "=")
-    bool set(const char* config, bool immutable=false);
-
     // - Set named parameter to value, with name truncated at len
     bool set(const char* name, int len,
                   const char* val, bool immutable);
@@ -88,6 +85,11 @@ namespace rfb {
 
     // - Remove a parameter from this Configuration group
     bool remove(const char* param);
+
+    // - handleArg
+    //   Parse a command line argument into a parameter, returning how
+    //   many arguments were consumed
+    int handleArg(int argc, char* argv[], int index);
 
 
     // - Get the Global Configuration object
@@ -105,9 +107,6 @@ namespace rfb {
     static bool setParam(const char* param, const char* value, bool immutable=false) {
       return global()->set(param, value, immutable);
     }
-    static bool setParam(const char* config, bool immutable=false) { 
-      return global()->set(config, immutable);
-    }
     static bool setParam(const char* name, int len,
       const char* val, bool immutable) {
       return global()->set(name, len, val, immutable);
@@ -118,6 +117,9 @@ namespace rfb {
     }
     static bool removeParam(const char* param) {
       return global()->remove(param);
+    }
+    static int handleParamArg(int argc, char* argv[], int index) {
+      return global()->handleArg(argc, argv, index);
     }
 
   private:

tests/perf/encperf.cxx
@@ -433,18 +433,16 @@ int main(int argc, char **argv)
 
   fn = nullptr;
   for (i = 1; i < argc; i++) {
-    if (rfb::Configuration::setParam(argv[i]))
-      continue;
+    int ret;
 
-    if (argv[i][0] == '-') {
-      if (i + 1 < argc) {
-        if (rfb::Configuration::setParam(&argv[i][1], argv[i + 1])) {
-          i++;
-          continue;
-        }
-      }
-      usage(argv[0]);
+    ret = rfb::Configuration::handleParamArg(argc, argv, i);
+    if (ret > 0) {
+      i += ret;
+      continue;
     }
+
+    if (argv[i][0] == '-')
+      usage(argv[0]);
 
     if (fn != nullptr)
       usage(argv[0]);

tests/unit/CMakeLists.txt
@@ -1,6 +1,9 @@
 include_directories(${CMAKE_SOURCE_DIR}/common)
 include_directories(${CMAKE_SOURCE_DIR}/vncviewer)
 
+add_executable(configargs configargs.cxx)
+target_link_libraries(configargs rfb)
+
 add_executable(conv conv.cxx)
 target_link_libraries(conv rfb)
 

unix/vncconfig/vncconfig.cxx
@@ -213,15 +213,15 @@ int main(int argc, char** argv)
   // Process vncconfig's own parameters first, then we process the
   // other arguments when we have the X display.
   int i;
-  for (i = 1; i < argc; i++) {
-    if (Configuration::setParam(argv[i]))
-      continue;
+  for (i = 1; i < argc;) {
+    int ret;
 
-    if (argv[i][0] == '-' && i+1 < argc &&
-        Configuration::setParam(&argv[i][1], argv[i+1])) {
-      i++;
+    ret = Configuration::handleParamArg(argc, argv, i);
+    if (ret > 0) {
+      i += ret;
       continue;
     }
+
     break;
   }
 

unix/x0vncserver/x0vncserver.cxx
@@ -286,23 +286,19 @@ int main(int argc, char** argv)
     rfbport.setParam(-1);
 
   for (int i = 1; i < argc; i++) {
-    if (Configuration::setParam(argv[i]))
-      continue;
+    int ret;
 
-    if (argv[i][0] == '-') {
-      if (i+1 < argc) {
-        if (Configuration::setParam(&argv[i][1], argv[i+1])) {
-          i++;
-          continue;
-        }
-      }
-      if (strcmp(argv[i], "-v") == 0 ||
-          strcmp(argv[i], "-version") == 0 ||
-          strcmp(argv[i], "--version") == 0) {
-        printVersion(stdout);
-        return 0;
-      }
-      usage();
+    ret = Configuration::handleParamArg(argc, argv, i);
+    if (ret > 0) {
+      i += ret;
+      continue;
+    }
+
+    if (strcmp(argv[i], "-v") == 0 ||
+        strcmp(argv[i], "-version") == 0 ||
+        strcmp(argv[i], "--version") == 0) {
+      printVersion(stdout);
+      return 0;
     }
 
     usage();

unix/xserver/hw/vnc/RFBGlue.cc
@@ -107,11 +107,6 @@ int vncSetParam(const char *name, const char *value)
   }
 }
 
-int vncSetParamSimple(const char *nameAndValue)
-{
-  return rfb::Configuration::setParam(nameAndValue);
-}
-
 char* vncGetParam(const char *name)
 {
   VoidParameter *param;
@@ -199,6 +194,11 @@ char *vncGetParamList(void)
 void vncListParams(int width, int nameWidth)
 {
   rfb::Configuration::listParams(width, nameWidth);
+}
+
+int vncHandleParamArg(int argc, char* argv[], int index)
+{
+  return rfb::Configuration::handleParamArg(argc, argv, index);
 }
 
 int vncGetSocketPort(int fd)

commit 402f079920033631c14ec1a38194b57e9fb758c4

Fix handling of empty -parameter= arguments
Broken in e97e225.

common/rfb/Configuration.cxx
@@ -151,11 +151,11 @@ int Configuration::handleArg(int argc, char* argv[], int index)
       param = param.substr(1);
   } else {
     // All command line arguments need either an initial '-', or an '='
-    if (val.empty())
+    if (!equal)
       return 0;
   }
 
-  if (!val.empty())
+  if (equal)
     return set(param.c_str(), val.c_str()) ? 1 : 0;
 
   for (VoidParameter* current: params) {

tests/unit/configargs.cxx
@@ -105,6 +105,13 @@ static void test_none()
   ASSERT_EQ_I(ret, 1);
   ASSERT_EQ_S(strparam, "foo");
 
+  // empty string argument
+  argv = {"prog", "strparam=", "bar" };
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_S(strparam, "");
+
   // Bad parameter
   argv = {"prog", "fooparam=123"};
   ret = rfb::Configuration::handleParamArg(argv.size(),
@@ -152,6 +159,13 @@ static void test_single()
                                            (char**)argv.data(), 1);
   ASSERT_EQ_I(ret, 1);
   ASSERT_EQ_S(strparam, "foo");
+
+  // empty string argument with equals
+  argv = {"prog", "-strparam=", "bar" };
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_S(strparam, "");
 
   // Missing argument
   intparam.setParam(1);
@@ -214,6 +228,13 @@ static void test_double()
                                            (char**)argv.data(), 1);
   ASSERT_EQ_I(ret, 1);
   ASSERT_EQ_S(strparam, "foo");
+
+  // empty string argument with equals
+  argv = {"prog", "--strparam=", "bar" };
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_S(strparam, "");
 
   // Missing argument
   intparam.setParam(1);
@@ -292,6 +313,22 @@ static void test_bool()
                                            (char**)argv.data(), 1);
   ASSERT_EQ_I(ret, 1);
   ASSERT_EQ_I(boolparam, false);
+
+  // empty bool argument equals (single)
+  boolparam.setParam(true);
+  argv = {"prog", "-boolparam=", "on"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_I(boolparam, true);
+
+  // empty bool argument equals (double)
+  boolparam.setParam(true);
+  argv = {"prog", "--boolparam=", "on"};
+  ret = rfb::Configuration::handleParamArg(argv.size(),
+                                           (char**)argv.data(), 1);
+  ASSERT_EQ_I(ret, 1);
+  ASSERT_EQ_I(boolparam, true);
 
   // bool bad argument (single)
   boolparam.setParam(false);