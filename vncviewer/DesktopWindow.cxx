d07b3f85eb76bca390fb8907f5948b1a0074e033
Piotr Henryk Dabrowski <phd@phd.re>
2025-02-06 17:18:59 +0100

    Force grab/ungrab keyboard+mouse with Right Ctrl key

--- vncviewer/DesktopWindow.cxx
+++ vncviewer/DesktopWindow.cxx
@@ -85,7 +85,7 @@ DesktopWindow::DesktopWindow(int w, int h, const char *name,
     firstUpdate(true),
     delayedFullscreen(false), sentDesktopSize(false),
     pendingRemoteResize(false), lastResize({0, 0}),
-    keyboardGrabbed(false), mouseGrabbed(false),
+    keyboardGrabbed(false), mouseGrabbed(false), forceGrabbed(false),
     statsLastUpdates(0), statsLastPixels(0), statsLastPosition(0),
     statsGraph(nullptr)
 {
@@ -849,6 +849,17 @@ void DesktopWindow::updateOverlay(void *data)
   self->damage(FL_DAMAGE_USER1);
 }
 
+void DesktopWindow::toggleForceGrab() {
+  if (keyboardGrabbed && mouseGrabbed) {
+    ungrabPointer();
+    ungrabKeyboard();
+    forceGrabbed = false;
+  } else {
+    grabPointer();
+    grabKeyboard();
+    forceGrabbed = true;
+  }
+}
 
 int DesktopWindow::handle(int event)
 {
@@ -877,7 +888,9 @@ int DesktopWindow::handle(int event)
       // We don't get FL_LEAVE with a grabbed pointer, so check manually
       if ((Fl::event_x() < 0) || (Fl::event_x() >= w()) ||
           (Fl::event_y() < 0) || (Fl::event_y() >= h())) {
-        ungrabPointer();
+        if (!forceGrabbed) {
+          ungrabPointer();
+        }
       }
 #if !defined(WIN32) && !defined(__APPLE__)
       Window root, child;
@@ -888,7 +901,9 @@ int DesktopWindow::handle(int event)
       if (XQueryPointer(fl_display, fl_xid(this), &root, &child,
                         &x, &y, &wx, &wy, &mask) &&
           (root != XRootWindow(fl_display, fl_screen))) {
-        ungrabPointer();
+        if (!forceGrabbed) {
+          ungrabPointer();
+        }
       }
 #endif
     }
@@ -1180,6 +1195,7 @@ void DesktopWindow::ungrabKeyboard()
 {
   Fl::remove_timeout(handleGrab, this);
 
+  forceGrabbed = false;
   keyboardGrabbed = false;
 
   ungrabPointer();
@@ -1228,6 +1244,7 @@ void DesktopWindow::grabPointer()
 
 void DesktopWindow::ungrabPointer()
 {
+  forceGrabbed = false;
   mouseGrabbed = false;
 
 #if !defined(WIN32) && !defined(__APPLE__)
