f924000b65f9684cb7966ed12292f41bbf6ba067
Pierre Ossman <ossman@cendio.se>
2025-02-06 14:05:32 +0100

    Don't touch macOS window level if not shown
    Otherwise there isn't any system window to examine or modify.
    
    (cherry picked from commit 1280c7176cdd607b06beae0f16e79e1ceefe7410)

--- vncviewer/DesktopWindow.cxx
+++ vncviewer/DesktopWindow.cxx
@@ -1031,14 +1031,17 @@ void DesktopWindow::fullscreen_on()
   }
 #ifdef __APPLE__
   // This is a workaround for a bug in FLTK, see: https://github.com/fltk/fltk/pull/277
-  int savedLevel;
-  savedLevel = cocoa_get_level(this);
+  int savedLevel = -1;
+  if (shown())
+    savedLevel = cocoa_get_level(this);
 #endif
   fullscreen_screens(top, bottom, left, right);
 #ifdef __APPLE__
   // This is a workaround for a bug in FLTK, see: https://github.com/fltk/fltk/pull/277
-  if (cocoa_get_level(this) != savedLevel)
-    cocoa_set_level(this, savedLevel);
+  if (savedLevel != -1) {
+    if (cocoa_get_level(this) != savedLevel)
+      cocoa_set_level(this, savedLevel);
+  }
 #endif
 
   if (!fullscreen_active())
