379afd6c1c8c353998403c4bda56f277169a7078
Pierre Ossman <ossman@cendio.se>
2025-02-12 14:56:25 +0100

    Convert AllowOverride to StringListParameter

--- unix/xserver/hw/vnc/vncExtInit.cc
+++ unix/xserver/hw/vnc/vncExtInit.cc
@@ -27,7 +27,6 @@
 #include <sys/types.h>
 #include <pwd.h>
 
-#include <set>
 #include <string>
 
 #include <core/Configuration.h>
@@ -68,15 +67,6 @@ int vncFbstride[MAXSCREENS];
 
 int vncInetdSock = -1;
 
-struct CaseInsensitiveCompare {
-  bool operator() (const std::string &a, const std::string &b) const {
-    return strcasecmp(a.c_str(), b.c_str()) < 0;
-  }
-};
-
-typedef std::set<std::string, CaseInsensitiveCompare> ParamSet;
-static ParamSet allowOverrideSet;
-
 static const char* defaultDesktopName();
 
 core::IntParameter
@@ -100,12 +90,12 @@ core::BoolParameter
   avoidShiftNumLock("AvoidShiftNumLock",
                     "Avoid fake Shift presses for keys affected by "
                     "NumLock.", true);
-core::StringParameter
+core::StringListParameter
   allowOverride("AllowOverride",
                 "Comma separated list of parameters that can be "
                 "modified using VNC extension.",
-                "desktop,AcceptPointerEvents,SendCutText,AcceptCutText,"
-                "SendPrimary,SetPrimary");
+                {"desktop", "AcceptPointerEvents", "SendCutText",
+                 "AcceptCutText", "SendPrimary", "SetPrimary"});
 core::BoolParameter
   setPrimary("SetPrimary",
              "Set the PRIMARY as well as the CLIPBOARD selection",
@@ -169,19 +159,6 @@ static rfb::PixelFormat vncGetPixelFormat(int scrIdx)
                           redShift, greenShift, blueShift);
 }
 
-static void parseOverrideList(const char *text, ParamSet &out)
-{
-  for (const char* iter = text; ; ++iter) {
-    if (*iter == ',' || *iter == '\0') {
-      out.insert(std::string(text, iter));
-      text = iter + 1;
-
-      if (*iter == '\0')
-        break;
-    }
-  }
-}
-
 void vncExtensionInit(void)
 {
   if (vncExtGeneration == vncGetServerGeneration()) {
@@ -203,7 +180,6 @@ void vncExtensionInit(void)
     if (!initialised) {
       core::initStdIOLoggers();
 
-      parseOverrideList(allowOverride, allowOverrideSet);
       allowOverride.setImmutable();
 
       initialised = true;
@@ -525,8 +501,10 @@ void vncAbortMsc(int scrIdx, uint64_t id)
 
 int vncOverrideParam(const char *param, const char *value)
 {
-  if (allowOverrideSet.find(param) == allowOverrideSet.end())
-    return 0;
+  for (const char* allowed : allowOverride) {
+    if (strcasecmp(allowed, param) == 0)
+      return core::Configuration::setParam(param, value);
+  }
 
-  return core::Configuration::setParam(param, value);
+  return 0;
 }
