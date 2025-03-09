c4b5abc4d0a625da5d02bcd1057b9f8c9e7d2043
Piotr Henryk Dabrowski <phd@phd.re>
2025-03-09 13:12:08 +0100

    Option to toggle mouse+keyboard grab by middle-clicking on the window
    New option: GrabToggleWithMiddleButton

--- vncviewer/Viewport.cxx
+++ vncviewer/Viewport.cxx
@@ -645,6 +645,14 @@ void Viewport::handlePointerEvent(const core::Point& pos,
     }
   }
 
+  if (!viewOnly && grabToggleWithMiddleButton) {
+    if (buttonMask & 0x2) {
+      ((DesktopWindow*)window())->toggleForceGrab();
+      showCursor();
+      return;
+    }
+  }
+
   filterPointerEvent(pos, buttonMask);
 }
 
