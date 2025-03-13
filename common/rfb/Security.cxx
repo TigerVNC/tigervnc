TigerVNC/tigervnc

commit 4ff02ae2a2280cecf183d0eb81d94757369b34ae

Convert SecurityTypes to EnumListParameter

common/rfb/Security.cxx
@@ -24,6 +24,7 @@
 #include <string.h>
 
 #include <algorithm>
+#include <stdexcept>
 
 #include <core/LogWriter.h>
 #include <core/string.h>
@@ -44,9 +45,16 @@ Security::Security()
 {
 }
 
-Security::Security(core::StringParameter& secTypes)
+Security::Security(core::EnumListParameter &secTypes)
 {
-  enabledSecTypes = parseSecTypes(secTypes);
+  for (core::EnumListEntry type : secTypes) {
+    uint32_t typeNum = secTypeNum(type.getValueStr().c_str());
+    // Should have been filtered by EnumListParameter, but let's have
+    // a safety net
+    if (typeNum != secTypeInvalid)
+      throw std::logic_error("Unknown security type");
+    enabledSecTypes.push_back(typeNum);
+  }
 }
 
 const std::list<uint8_t> Security::GetEnabledSecTypes(void)
@@ -179,17 +187,4 @@ const char* rfb::secTypeName(uint32_t num)
   case secTypeX509Plain:  return "X509Plain";
   default:                return "[unknown secType]";
   }
-}
-
-std::list<uint32_t> rfb::parseSecTypes(const char* types_)
-{
-  std::list<uint32_t> result;
-  std::vector<std::string> types;
-  types = core::split(types_, ',');
-  for (size_t i = 0; i < types.size(); i++) {
-    uint32_t typeNum = secTypeNum(types[i].c_str());
-    if (typeNum != secTypeInvalid)
-      result.push_back(typeNum);
-  }
-  return result;
 }

common/rfb/Security.h
@@ -26,7 +26,10 @@
 
 #include <list>
 
-namespace core { class StringParameter; }
+namespace core {
+  class EnumListParameter;
+  class StringParameter;
+}
 
 namespace rfb {
 
@@ -77,7 +80,7 @@ namespace rfb {
      * Create Security instance.
      */
     Security();
-    Security(core::StringParameter& secTypes);
+    Security(core::EnumListParameter& secTypes);
 
     /*
      * Note about security types.
@@ -115,7 +118,6 @@ namespace rfb {
 
   const char* secTypeName(uint32_t num);
   uint32_t secTypeNum(const char* name);
-  std::list<uint32_t> parseSecTypes(const char* types);
 
 }
 

common/rfb/SecurityClient.cxx
@@ -45,7 +45,7 @@
 
 using namespace rfb;
 
-core::StringParameter SecurityClient::secTypes
+core::EnumListParameter SecurityClient::secTypes
 ("SecurityTypes",
  "Specify which security scheme to use (None, VncAuth, Plain"
 #ifdef HAVE_GNUTLS
@@ -55,13 +55,22 @@ core::StringParameter SecurityClient::secTypes
  ", RA2, RA2ne, RA2_256, RA2ne_256, DH, MSLogonII"
 #endif
  ")",
+ { "None", "VncAuth", "Plain",
 #ifdef HAVE_GNUTLS
- "X509Plain,TLSPlain,X509Vnc,TLSVnc,X509None,TLSNone,"
+ "TLSNone", "TLSVnc", "TLSPlain", "X509None", "X509Vnc", "X509Plain",
 #endif
 #ifdef HAVE_NETTLE
- "RA2,RA2_256,RA2ne,RA2ne_256,DH,MSLogonII,"
+ "RA2", "RA2ne", "RA2_256", "RA2ne_256", "DH", "MSLogonII",
 #endif
- "Plain,VncAuth,None");
+ },
+ { "None", "VncAuth", "Plain",
+#ifdef HAVE_GNUTLS
+ "TLSNone", "TLSVnc", "TLSPlain", "X509None", "X509Vnc", "X509Plain",
+#endif
+#ifdef HAVE_NETTLE
+ "RA2", "RA2ne", "RA2_256", "RA2ne_256", "DH", "MSLogonII",
+#endif
+ });
 
 CSecurity* SecurityClient::GetCSecurity(CConnection* cc, uint32_t secType)
 {

common/rfb/SecurityClient.h
@@ -24,8 +24,6 @@
 
 #include <rfb/Security.h>
 
-namespace core { class StringParameter; }
-
 namespace rfb {
 
   class CConnection;
@@ -38,7 +36,7 @@ namespace rfb {
     /* Create client side CSecurity class instance */
     CSecurity* GetCSecurity(CConnection* cc, uint32_t secType);
 
-    static core::StringParameter secTypes;
+    static core::EnumListParameter secTypes;
   };
 
 }

common/rfb/SecurityServer.cxx
@@ -40,7 +40,7 @@
 
 using namespace rfb;
 
-core::StringParameter SecurityServer::secTypes
+core::EnumListParameter SecurityServer::secTypes
 ("SecurityTypes",
  "Specify which security scheme to use (None, VncAuth, Plain"
 #ifdef HAVE_GNUTLS
@@ -50,10 +50,19 @@ core::StringParameter SecurityServer::secTypes
  ", RA2, RA2ne, RA2_256, RA2ne_256"
 #endif
  ")",
+ { "None", "VncAuth", "Plain",
 #ifdef HAVE_GNUTLS
- "TLSVnc,"
+ "TLSNone", "TLSVnc", "TLSPlain", "X509None", "X509Vnc", "X509Plain",
 #endif
- "VncAuth");
+#ifdef HAVE_NETTLE
+ "RA2", "RA2ne", "RA2_256", "RA2ne_256",
+#endif
+ },
+ {
+#ifdef HAVE_GNUTLS
+ "TLSVnc",
+#endif
+ "VncAuth"});
 
 SSecurity* SecurityServer::GetSSecurity(SConnection* sc, uint32_t secType)
 {

common/rfb/SecurityServer.h
@@ -34,7 +34,7 @@ namespace rfb {
     /* Create server side SSecurity class instance */
     SSecurity* GetSSecurity(SConnection* sc, uint32_t secType);
 
-    static core::StringParameter secTypes;
+    static core::EnumListParameter secTypes;
   };
 
 }

commit ac0d1fe8a310ba15d9959be4d0fbeeb68c69dc81

Fix bad logic in security types handling
Broken in 4ff02ae.

common/rfb/Security.cxx
@@ -51,7 +51,7 @@ Security::Security(core::EnumListParameter &secTypes)
     uint32_t typeNum = secTypeNum(type.getValueStr().c_str());
     // Should have been filtered by EnumListParameter, but let's have
     // a safety net
-    if (typeNum != secTypeInvalid)
+    if (typeNum == secTypeInvalid)
       throw std::logic_error("Unknown security type");
     enabledSecTypes.push_back(typeNum);
   }