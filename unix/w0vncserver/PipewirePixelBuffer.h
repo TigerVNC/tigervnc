85f341b89b24bd649618489a11373de488ae6e8a
Adam Halim <adaha@cendio.se>
2025-05-23 13:26:55 +0200

    w0vncserver: Add local cursor support
    By setting 'cursor_mode' to METADATA, PipeWire will send cursor metadata
    in the same stream as screen content instead of having it embedded in
    the screen buffer.
    
    I saw some issues with enabling cursor metadata with mutter where some
    frames would be skipped. Let's leave this as an option that is disabled
    by default until things get fixed.

--- unix/w0vncserver/PipewirePixelBuffer.h
+++ unix/w0vncserver/PipewirePixelBuffer.h
@@ -21,6 +21,17 @@ struct PipeWireData {
     uint32_t h;
   };
 
+  struct Cursor {
+    uint32_t w;
+    uint32_t h;
+    int32_t x;
+    int32_t y;
+    int32_t hotspotX;
+    int32_t hotspotY;
+    int32_t stride;
+    uint8_t *data;
+  };
+
   spa_io_position* position;
   spa_io_clock* clock;
 
@@ -30,6 +41,7 @@ struct PipeWireData {
   Rect rect;
 
   uint8_t *buffer;
+  Cursor cursor;
 };
 
 class PipeWirePixelBuffer;
