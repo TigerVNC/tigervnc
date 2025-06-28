04475d3ee7af4abea699290a2e85efb2c3c336cb
Pierre Ossman <ossman@cendio.se>
2025-06-28 20:11:11 +0200

    Add translations for server verification messages

--- common/rfb/CSecurityRSAAES.cxx
+++ common/rfb/CSecurityRSAAES.cxx
@@ -232,12 +232,13 @@ void CSecurityRSAAES::verifyServer()
   sha1_update(&ctx, serverKey.size, serverKeyN);
   sha1_update(&ctx, serverKey.size, serverKeyE);
   sha1_digest(&ctx, sizeof(f), f);
-  const char *title = "Server key fingerprint";
+  const char *title = _("Server encryption key fingerprint");
   std::string text = core::format(
-    "The server has provided the following identifying information:\n"
-    "Fingerprint: %02x-%02x-%02x-%02x-%02x-%02x-%02x-%02x\n"
-    "Please verify that the information is correct and press \"Yes\". "
-    "Otherwise press \"No\"", f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7]);
+    _("The server has provided the following identifying information:\n"
+      "Fingerprint: %02x-%02x-%02x-%02x-%02x-%02x-%02x-%02x\n"
+      "Please verify that the information is correct and press "
+      "\"Yes\". Otherwise press \"No\""),
+    f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7]);
   if (!cc->showMsgBox(MsgBoxFlags::M_YESNO, title, text.c_str()))
     throw auth_cancelled();
 }
