dbe3f1e48b6fce8098d85e3cfc35282533e573e2
Pierre Ossman <ossman@cendio.se>
2025-07-09 13:18:51 +0200

    Remove barely used LogWriter::status()
    It's not clear when this should be used and not info(), so let's just
    remove the confusion.

--- unix/x0vncserver/x0vncserver.cxx
+++ unix/x0vncserver/x0vncserver.cxx
@@ -475,7 +475,7 @@ int main(int argc, char** argv)
           if (sock) {
             server.addSocket(sock);
           } else {
-            vlog.status("Client connection rejected");
+            vlog.info("Client connection rejected");
           }
         }
       }
