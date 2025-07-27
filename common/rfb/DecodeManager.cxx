TigerVNC/tigervnc

commit ad0f0618fa2ca13d7b916f22eccc5ba3201482cb

Change streams to be asynchronous
Major restructuring of how streams work. Neither input nor output
streams are now blocking. This avoids stalling the rest of the client or
server when a peer is slow or unresponsive.

Note that this puts an extra burden on users of streams to make sure
they are allowed to do their work once the underlying transports are
ready (e.g. monitoring fds).

common/rfb/DecodeManager.cxx
@@ -103,7 +103,7 @@ DecodeManager::~DecodeManager()
     delete decoders[i];
 }
 
-void DecodeManager::decodeRect(const Rect& r, int encoding,
+bool DecodeManager::decodeRect(const Rect& r, int encoding,
                                ModifiablePixelBuffer* pb)
 {
   Decoder *decoder;
@@ -133,19 +133,21 @@ void DecodeManager::decodeRect(const Rect& r, int encoding,
   if (threads.empty()) {
     bufferStream = freeBuffers.front();
     bufferStream->clear();
-    decoder->readRect(r, conn->getInStream(), conn->server, bufferStream);
+    if (!decoder->readRect(r, conn->getInStream(), conn->server, bufferStream))
+      return false;
     try {
       decoder->decodeRect(r, bufferStream->data(), bufferStream->length(),
                           conn->server, pb);
     } catch (rdr::Exception& e) {
       throw Exception("Error decoding rect: %s", e.str());
     }
-    return;
+    return true;
   }
 
   // Wait for an available memory buffer
   queueMutex->lock();
 
+  // FIXME: Should we return and let other things run here?
   while (freeBuffers.empty())
     producerCond->wait();
 
@@ -160,7 +162,8 @@ void DecodeManager::decodeRect(const Rect& r, int encoding,
 
   // Read the rect
   bufferStream->clear();
-  decoder->readRect(r, conn->getInStream(), conn->server, bufferStream);
+  if (!decoder->readRect(r, conn->getInStream(), conn->server, bufferStream))
+    return false;
 
   // Then try to put it on the queue
   entry = new QueueEntry;
@@ -190,6 +193,8 @@ void DecodeManager::decodeRect(const Rect& r, int encoding,
   consumerCond->signal();
 
   queueMutex->unlock();
+
+  return true;
 }
 
 void DecodeManager::flush()

common/rfb/DecodeManager.h
@@ -47,7 +47,7 @@ namespace rfb {
     DecodeManager(CConnection *conn);
     ~DecodeManager();
 
-    void decodeRect(const Rect& r, int encoding,
+    bool decodeRect(const Rect& r, int encoding,
                     ModifiablePixelBuffer* pb);
 
     void flush();

common/rfb/HextileDecoder.cxx
@@ -44,11 +44,13 @@ HextileDecoder::~HextileDecoder()
 {
 }
 
-void HextileDecoder::readRect(const Rect& r, rdr::InStream* is,
+bool HextileDecoder::readRect(const Rect& r, rdr::InStream* is,
                               const ServerParams& server, rdr::OutStream* os)
 {
   Rect t;
   size_t bytesPerPixel;
+
+  is->setRestorePoint();
 
   bytesPerPixel = server.pf().bpp/8;
 
@@ -61,33 +63,57 @@ void HextileDecoder::readRect(const Rect& r, rdr::InStream* is,
 
       t.br.x = __rfbmin(r.br.x, t.tl.x + 16);
 
+      if (!is->hasDataOrRestore(1))
+        return false;
+
       tileType = is->readU8();
       os->writeU8(tileType);
 
       if (tileType & hextileRaw) {
+        if (!is->hasDataOrRestore(t.area() * bytesPerPixel))
+          return false;
         os->copyBytes(is, t.area() * bytesPerPixel);
         continue;
       }
 
-      if (tileType & hextileBgSpecified)
-        os->copyBytes(is, bytesPerPixel);
 
-      if (tileType & hextileFgSpecified)
+      if (tileType & hextileBgSpecified) {
+        if (!is->hasDataOrRestore(bytesPerPixel))
+          return false;
         os->copyBytes(is, bytesPerPixel);
+      }
+
+      if (tileType & hextileFgSpecified) {
+        if (!is->hasDataOrRestore(bytesPerPixel))
+          return false;
+        os->copyBytes(is, bytesPerPixel);
+      }
 
       if (tileType & hextileAnySubrects) {
         rdr::U8 nSubrects;
 
+        if (!is->hasDataOrRestore(1))
+          return false;
+
         nSubrects = is->readU8();
         os->writeU8(nSubrects);
 
-        if (tileType & hextileSubrectsColoured)
+        if (tileType & hextileSubrectsColoured) {
+          if (!is->hasDataOrRestore(nSubrects * (bytesPerPixel + 2)))
+            return false;
           os->copyBytes(is, nSubrects * (bytesPerPixel + 2));
-        else
+        } else {
+          if (!is->hasDataOrRestore(nSubrects * 2))
+            return false;
           os->copyBytes(is, nSubrects * 2);
+        }
       }
     }
   }
+
+  is->clearRestorePoint();
+
+  return true;
 }
 
 void HextileDecoder::decodeRect(const Rect& r, const void* buffer,

common/rfb/HextileDecoder.h
@@ -26,7 +26,7 @@ namespace rfb {
   public:
     HextileDecoder();
     virtual ~HextileDecoder();
-    virtual void readRect(const Rect& r, rdr::InStream* is,
+    virtual bool readRect(const Rect& r, rdr::InStream* is,
                           const ServerParams& server, rdr::OutStream* os);
     virtual void decodeRect(const Rect& r, const void* buffer,
                             size_t buflen, const ServerParams& server,

common/rfb/RREDecoder.cxx
@@ -44,15 +44,30 @@ RREDecoder::~RREDecoder()
 {
 }
 
-void RREDecoder::readRect(const Rect& r, rdr::InStream* is,
+bool RREDecoder::readRect(const Rect& r, rdr::InStream* is,
                           const ServerParams& server, rdr::OutStream* os)
 {
   rdr::U32 numRects;
+  size_t len;
+
+  if (!is->hasData(4))
+    return false;
+
+  is->setRestorePoint();
 
   numRects = is->readU32();
   os->writeU32(numRects);
 
-  os->copyBytes(is, server.pf().bpp/8 + numRects * (server.pf().bpp/8 + 8));
+  len = server.pf().bpp/8 + numRects * (server.pf().bpp/8 + 8);
+
+  if (!is->hasDataOrRestore(len))
+    return false;
+
+  is->clearRestorePoint();
+
+  os->copyBytes(is, len);
+
+  return true;
 }
 
 void RREDecoder::decodeRect(const Rect& r, const void* buffer,

common/rfb/TightDecoder.cxx
@@ -54,10 +54,15 @@ TightDecoder::~TightDecoder()
 {
 }
 
-void TightDecoder::readRect(const Rect& r, rdr::InStream* is,
+bool TightDecoder::readRect(const Rect& r, rdr::InStream* is,
                             const ServerParams& server, rdr::OutStream* os)
 {
   rdr::U8 comp_ctl;
+
+  if (!is->hasData(1))
+    return false;
+
+  is->setRestorePoint();
 
   comp_ctl = is->readU8();
   os->writeU8(comp_ctl);
@@ -66,21 +71,38 @@ void TightDecoder::readRect(const Rect& r, rdr::InStream* is,
 
   // "Fill" compression type.
   if (comp_ctl == tightFill) {
-    if (server.pf().is888())
+    if (server.pf().is888()) {
+      if (!is->hasDataOrRestore(3))
+        return false;
       os->copyBytes(is, 3);
-    else
+    } else {
+      if (!is->hasDataOrRestore(server.pf().bpp/8))
+        return false;
       os->copyBytes(is, server.pf().bpp/8);
-    return;
+    }
+    is->clearRestorePoint();
+    return true;
   }
 
   // "JPEG" compression type.
   if (comp_ctl == tightJpeg) {
     rdr::U32 len;
 
+    // FIXME: Might be less than 3 bytes
+    if (!is->hasDataOrRestore(3))
+      return false;
+
     len = readCompact(is);
     os->writeOpaque32(len);
+
+    if (!is->hasDataOrRestore(len))
+      return false;
+
     os->copyBytes(is, len);
-    return;
+
+    is->clearRestorePoint();
+
+    return true;
   }
 
   // Quit on unsupported compression type.
@@ -98,18 +120,29 @@ void TightDecoder::readRect(const Rect& r, rdr::InStream* is,
   if ((comp_ctl & tightExplicitFilter) != 0) {
     rdr::U8 filterId;
 
+    if (!is->hasDataOrRestore(1))
+      return false;
+
     filterId = is->readU8();
     os->writeU8(filterId);
 
     switch (filterId) {
     case tightFilterPalette:
+      if (!is->hasDataOrRestore(1))
+        return false;
+
       palSize = is->readU8() + 1;
       os->writeU8(palSize - 1);
 
-      if (server.pf().is888())
+      if (server.pf().is888()) {
+        if (!is->hasDataOrRestore(palSize * 3))
+          return false;
         os->copyBytes(is, palSize * 3);
-      else
+      } else {
+        if (!is->hasDataOrRestore(palSize * server.pf().bpp/8))
+          return false;
         os->copyBytes(is, palSize * server.pf().bpp/8);
+      }
       break;
     case tightFilterGradient:
       if (server.pf().bpp == 8)
@@ -137,15 +170,29 @@ void TightDecoder::readRect(const Rect& r, rdr::InStream* is,
 
   dataSize = r.height() * rowSize;
 
-  if (dataSize < TIGHT_MIN_TO_COMPRESS)
+  if (dataSize < TIGHT_MIN_TO_COMPRESS) {
+    if (!is->hasDataOrRestore(dataSize))
+      return false;
     os->copyBytes(is, dataSize);
-  else {
+  } else {
     rdr::U32 len;
+
+    // FIXME: Might be less than 3 bytes
+    if (!is->hasDataOrRestore(3))
+      return false;
 
     len = readCompact(is);
     os->writeOpaque32(len);
+
+    if (!is->hasDataOrRestore(len))
+      return false;
+
     os->copyBytes(is, len);
   }
+
+  is->clearRestorePoint();
+
+  return true;
 }
 
 bool TightDecoder::doRectsConflict(const Rect& rectA,
@@ -339,6 +386,8 @@ void TightDecoder::decodeRect(const Rect& r, const void* buffer,
     // Allocate buffer and decompress the data
     netbuf = new rdr::U8[dataSize];
 
+    if (!zis[streamId].hasData(dataSize))
+      throw Exception("Tight decode error");
     zis[streamId].readBytes(netbuf, dataSize);
 
     zis[streamId].flushUnderlying();

common/rfb/TightDecoder.h
@@ -31,7 +31,7 @@ namespace rfb {
   public:
     TightDecoder();
     virtual ~TightDecoder();
-    virtual void readRect(const Rect& r, rdr::InStream* is,
+    virtual bool readRect(const Rect& r, rdr::InStream* is,
                           const ServerParams& server, rdr::OutStream* os);
     virtual bool doRectsConflict(const Rect& rectA,
                                  const void* bufferA,

common/rfb/ZRLEDecoder.cxx
@@ -21,6 +21,7 @@
 #include <rdr/MemInStream.h>
 #include <rdr/OutStream.h>
 
+#include <rfb/Exception.h>
 #include <rfb/ServerParams.h>
 #include <rfb/PixelBuffer.h>
 #include <rfb/ZRLEDecoder.h>
@@ -29,7 +30,6 @@ using namespace rfb;
 
 static inline rdr::U32 readOpaque24A(rdr::InStream* is)
 {
-  is->check(3);
   rdr::U32 r=0;
   ((rdr::U8*)&r)[0] = is->readU8();
   ((rdr::U8*)&r)[1] = is->readU8();
@@ -39,12 +39,17 @@ static inline rdr::U32 readOpaque24A(rdr::InStream* is)
 }
 static inline rdr::U32 readOpaque24B(rdr::InStream* is)
 {
-  is->check(3);
   rdr::U32 r=0;
   ((rdr::U8*)&r)[1] = is->readU8();
   ((rdr::U8*)&r)[2] = is->readU8();
   ((rdr::U8*)&r)[3] = is->readU8();
   return r;
+}
+
+static inline void zlibHasData(rdr::ZlibInStream* zis, size_t length)
+{
+  if (!zis->hasData(length))
+    throw Exception("ZRLE decode error");
 }
 
 #define BPP 8
@@ -71,14 +76,27 @@ ZRLEDecoder::~ZRLEDecoder()
 {
 }
 
-void ZRLEDecoder::readRect(const Rect& r, rdr::InStream* is,
+bool ZRLEDecoder::readRect(const Rect& r, rdr::InStream* is,
                            const ServerParams& server, rdr::OutStream* os)
 {
   rdr::U32 len;
 
+  if (!is->hasData(4))
+    return false;
+
+  is->setRestorePoint();
+
   len = is->readU32();
   os->writeU32(len);
+
+  if (!is->hasDataOrRestore(len))
+    return false;
+
+  is->clearRestorePoint();
+
   os->copyBytes(is, len);
+
+  return true;
 }
 
 void ZRLEDecoder::decodeRect(const Rect& r, const void* buffer,

commit f83c6060a3ecf01d3e45b10e4d5ca93488716de7

Handle partial reads in decoders correctly
Don't assume that decoders will back up the input stream completely on
partial data. I.e. don't drop the output stream data on each retry.

This has technically been broken since ad0f0618, but has worked in
practice because all current decoder never actually did a partial read.

As of this commit, the Hextile and Tight decoder now both do partial
reads, making them slightly more efficient.

common/rfb/DecodeManager.cxx
@@ -40,7 +40,7 @@ using namespace rfb;
 static core::LogWriter vlog("DecodeManager");
 
 DecodeManager::DecodeManager(CConnection *conn_) :
-  conn(conn_), threadException(nullptr)
+  conn(conn_), partialEntry(nullptr), threadException(nullptr)
 {
   size_t cpuCount;
 
@@ -96,75 +96,86 @@ DecodeManager::~DecodeManager()
 
   for (Decoder* decoder : decoders)
     delete decoder;
+
+  delete partialEntry;
 }
 
 bool DecodeManager::decodeRect(const core::Rect& r, int encoding,
                                ModifiablePixelBuffer* pb)
 {
-  Decoder *decoder;
-  rdr::MemOutStream *bufferStream;
   int equiv;
-
-  QueueEntry *entry;
 
   assert(pb != nullptr);
 
-  if (!Decoder::supported(encoding)) {
-    vlog.error("Unknown encoding %d", encoding);
-    throw protocol_error("Unknown encoding");
-  }
+  if (partialEntry == nullptr) {
+    Decoder *decoder;
+    rdr::MemOutStream *bufferStream;
 
-  if (!decoders[encoding]) {
-    decoders[encoding] = Decoder::createDecoder(encoding);
-    if (!decoders[encoding]) {
+    if (!Decoder::supported(encoding)) {
       vlog.error("Unknown encoding %d", encoding);
       throw protocol_error("Unknown encoding");
     }
+
+    if (!decoders[encoding]) {
+      decoders[encoding] = Decoder::createDecoder(encoding);
+      if (!decoders[encoding]) {
+        vlog.error("Unknown encoding %d", encoding);
+        throw protocol_error("Unknown encoding");
+      }
+    }
+
+    decoder = decoders[encoding];
+
+    // Wait for an available memory buffer
+    queueMutex->lock();
+
+    // FIXME: Should we return and let other things run here?
+    while (freeBuffers.empty())
+      producerCond->wait();
+
+    // Don't pop the buffer as we don't know if we'll finish filling it
+    // in a single round
+    bufferStream = freeBuffers.front();
+    bufferStream->clear();
+
+    queueMutex->unlock();
+
+    partialEntry = new QueueEntry();
+
+    partialEntry->active = false;
+    partialEntry->rect = r;
+    partialEntry->encoding = encoding;
+    partialEntry->decoder = decoder;
+    partialEntry->server = &conn->server;
+    partialEntry->pb = pb;
+    partialEntry->bufferStream = bufferStream;
+  } else {
+    assert(partialEntry->rect == r);
+    assert(partialEntry->encoding == encoding);
+    assert(partialEntry->pb == pb);
   }
-
-  decoder = decoders[encoding];
-
-  // Wait for an available memory buffer
-  queueMutex->lock();
-
-  // FIXME: Should we return and let other things run here?
-  while (freeBuffers.empty())
-    producerCond->wait();
-
-  // Don't pop the buffer in case we throw an exception
-  // whilst reading
-  bufferStream = freeBuffers.front();
-
-  queueMutex->unlock();
 
   // First check if any thread has encountered a problem
   throwThreadException();
 
   // Read the rect
-  bufferStream->clear();
-  if (!decoder->readRect(r, conn->getInStream(), conn->server, bufferStream))
+  if (!partialEntry->decoder->readRect(
+        partialEntry->rect, conn->getInStream(), conn->server,
+        partialEntry->bufferStream))
     return false;
 
+  partialEntry->decoder->getAffectedRegion(
+    r, partialEntry->bufferStream->data(),
+    partialEntry->bufferStream->length(), conn->server,
+    &partialEntry->affectedRegion);
+
   stats[encoding].rects++;
-  stats[encoding].bytes += 12 + bufferStream->length();
+  stats[encoding].bytes += 12 + partialEntry->bufferStream->length();
   stats[encoding].pixels += r.area();
   equiv = 12 + r.area() * (conn->server.pf().bpp/8);
   stats[encoding].equivalent += equiv;
 
   // Then try to put it on the queue
-  entry = new QueueEntry;
-
-  entry->active = false;
-  entry->rect = r;
-  entry->encoding = encoding;
-  entry->decoder = decoder;
-  entry->server = &conn->server;
-  entry->pb = pb;
-  entry->bufferStream = bufferStream;
-
-  decoder->getAffectedRegion(r, bufferStream->data(),
-                             bufferStream->length(), conn->server,
-                             &entry->affectedRegion);
 
   queueMutex->lock();
 
@@ -172,7 +183,8 @@ bool DecodeManager::decodeRect(const core::Rect& r, int encoding,
   // the front is still the same buffer
   freeBuffers.pop_front();
 
-  workQueue.push_back(entry);
+  workQueue.push_back(partialEntry);
+  partialEntry = nullptr;
 
   // We only put a single entry on the queue so waking a single
   // thread is sufficient

common/rfb/DecodeManager.h
@@ -85,6 +85,7 @@ namespace rfb {
 
     std::list<rdr::MemOutStream*> freeBuffers;
     std::list<QueueEntry*> workQueue;
+    QueueEntry* partialEntry;
 
     core::Mutex* queueMutex;
     core::Condition* producerCond;

common/rfb/H264Decoder.cxx
@@ -76,16 +76,15 @@ bool H264Decoder::readRect(const core::Rect& /*r*/,
   is->setRestorePoint();
 
   len = is->readU32();
-  os->writeU32(len);
   uint32_t reset = is->readU32();
-
-  os->writeU32(reset);
 
   if (!is->hasDataOrRestore(len))
     return false;
 
   is->clearRestorePoint();
 
+  os->writeU32(len);
+  os->writeU32(reset);
   os->copyBytes(is, len);
 
   return true;

common/rfb/HextileDecoder.cxx
@@ -35,7 +35,8 @@
 
 using namespace rfb;
 
-HextileDecoder::HextileDecoder() : Decoder(DecoderPlain)
+HextileDecoder::HextileDecoder()
+  : Decoder(DecoderPlain), readTile(0, 0)
 {
 }
 
@@ -46,71 +47,88 @@ HextileDecoder::~HextileDecoder()
 bool HextileDecoder::readRect(const core::Rect& r, rdr::InStream* is,
                               const ServerParams& server, rdr::OutStream* os)
 {
-  core::Rect t;
   size_t bytesPerPixel;
-
-  is->setRestorePoint();
 
   bytesPerPixel = server.pf().bpp/8;
 
-  for (t.tl.y = r.tl.y; t.tl.y < r.br.y; t.tl.y += 16) {
+  for (; readTile.y < r.height(); readTile.y += 16) {
+    int height;
 
-    t.br.y = std::min(r.br.y, t.tl.y + 16);
+    height = std::min(r.height() - readTile.y, 16);
 
-    for (t.tl.x = r.tl.x; t.tl.x < r.br.x; t.tl.x += 16) {
+    for (; readTile.x < r.width(); readTile.x += 16) {
+      int width;
+
       uint8_t tileType;
+      uint8_t bg[4], fg[4];
+      uint8_t nSubrects;
 
-      t.br.x = std::min(r.br.x, t.tl.x + 16);
+      width = std::min(r.width() - readTile.x, 16);
 
-      if (!is->hasDataOrRestore(1))
+      if (!is->hasData(1))
         return false;
 
+      is->setRestorePoint();
+
       tileType = is->readU8();
-      os->writeU8(tileType);
 
       if (tileType & hextileRaw) {
-        if (!is->hasDataOrRestore(t.area() * bytesPerPixel))
+        if (!is->hasDataOrRestore(width * height * bytesPerPixel))
           return false;
-        os->copyBytes(is, t.area() * bytesPerPixel);
+        os->writeU8(tileType);
+        os->copyBytes(is, width * height * bytesPerPixel);
+        is->clearRestorePoint();
         continue;
       }
-
 
       if (tileType & hextileBgSpecified) {
         if (!is->hasDataOrRestore(bytesPerPixel))
           return false;
-        os->copyBytes(is, bytesPerPixel);
+        is->readBytes(bg, bytesPerPixel);
       }
 
       if (tileType & hextileFgSpecified) {
         if (!is->hasDataOrRestore(bytesPerPixel))
           return false;
-        os->copyBytes(is, bytesPerPixel);
+        is->readBytes(fg, bytesPerPixel);
       }
 
+      nSubrects = 0;
       if (tileType & hextileAnySubrects) {
-        uint8_t nSubrects;
-
         if (!is->hasDataOrRestore(1))
           return false;
 
         nSubrects = is->readU8();
-        os->writeU8(nSubrects);
 
         if (tileType & hextileSubrectsColoured) {
           if (!is->hasDataOrRestore(nSubrects * (bytesPerPixel + 2)))
             return false;
-          os->copyBytes(is, nSubrects * (bytesPerPixel + 2));
         } else {
           if (!is->hasDataOrRestore(nSubrects * 2))
             return false;
-          os->copyBytes(is, nSubrects * 2);
         }
       }
+
+      os->writeU8(tileType);
+      if (tileType & hextileBgSpecified)
+        os->writeBytes(bg, bytesPerPixel);
+      if (tileType & hextileFgSpecified)
+        os->writeBytes(fg, bytesPerPixel);
+      if (tileType & hextileAnySubrects) {
+        os->writeU8(nSubrects);
+        if (tileType & hextileSubrectsColoured)
+          os->copyBytes(is, nSubrects * (bytesPerPixel + 2));
+        else
+          os->copyBytes(is, nSubrects * 2);
+      }
+
+      is->clearRestorePoint();
     }
+
+    readTile.x = 0;
   }
 
-  is->clearRestorePoint();
+  readTile.y = 0;
 
   return true;
 }

common/rfb/HextileDecoder.h
@@ -42,6 +42,9 @@ namespace rfb {
     void hextileDecode(const core::Rect& r, rdr::InStream* is,
                        const PixelFormat& pf,
                        ModifiablePixelBuffer* pb);
+
+  private:
+    core::Point readTile;
   };
 }
 #endif

common/rfb/RREDecoder.cxx
@@ -52,7 +52,6 @@ bool RREDecoder::readRect(const core::Rect& /*r*/, rdr::InStream* is,
   is->setRestorePoint();
 
   numRects = is->readU32();
-  os->writeU32(numRects);
 
   len = server.pf().bpp/8 + numRects * (server.pf().bpp/8 + 8);
 
@@ -61,6 +60,7 @@ bool RREDecoder::readRect(const core::Rect& /*r*/, rdr::InStream* is,
 
   is->clearRestorePoint();
 
+  os->writeU32(numRects);
   os->copyBytes(is, len);
 
   return true;

common/rfb/TightDecoder.cxx
@@ -45,7 +45,8 @@ using namespace rfb;
 static const int TIGHT_MAX_WIDTH = 2048;
 static const int TIGHT_MIN_TO_COMPRESS = 12;
 
-TightDecoder::TightDecoder() : Decoder(DecoderPartiallyOrdered)
+TightDecoder::TightDecoder()
+  : Decoder(DecoderPartiallyOrdered), readState(IDLE)
 {
 }
 
@@ -56,71 +57,84 @@ TightDecoder::~TightDecoder()
 bool TightDecoder::readRect(const core::Rect& r, rdr::InStream* is,
                             const ServerParams& server, rdr::OutStream* os)
 {
-  uint8_t comp_ctl;
+  if (readState == IDLE) {
+    uint8_t comp_ctl;
 
-  if (!is->hasData(1))
-    return false;
+    if (r.width() > TIGHT_MAX_WIDTH)
+      throw protocol_error(core::format(
+        "TightDecoder: Too large rectangle (%d pixels)", r.width()));
 
-  is->setRestorePoint();
+    if (!is->hasData(1))
+      return false;
 
-  comp_ctl = is->readU8();
-  os->writeU8(comp_ctl);
+    comp_ctl = is->readU8();
+    os->writeU8(comp_ctl);
 
-  comp_ctl >>= 4;
+    comp_ctl >>= 4;
+
+    // Quit on unsupported compression type.
+    if (comp_ctl > tightMaxSubencoding)
+      throw protocol_error("TightDecoder: Bad subencoding value received");
+
+    if (comp_ctl == tightFill)
+      readState = FILL;
+    else if (comp_ctl == tightJpeg)
+      readState = JPEG;
+    else {
+      readPalSize = 0;
+      if ((comp_ctl & tightExplicitFilter) != 0)
+        readState = FILTER;
+      else
+        readState = PIXELS;
+    }
+  }
 
   // "Fill" compression type.
-  if (comp_ctl == tightFill) {
+  if (readState == FILL) {
     if (server.pf().is888()) {
-      if (!is->hasDataOrRestore(3))
+      if (!is->hasData(3))
         return false;
       os->copyBytes(is, 3);
     } else {
-      if (!is->hasDataOrRestore(server.pf().bpp/8))
+      if (!is->hasData(server.pf().bpp/8))
         return false;
       os->copyBytes(is, server.pf().bpp/8);
     }
-    is->clearRestorePoint();
+    readState = IDLE;
     return true;
   }
 
   // "JPEG" compression type.
-  if (comp_ctl == tightJpeg) {
+  if (readState == JPEG) {
     uint32_t len;
 
     // FIXME: Might be less than 3 bytes
-    if (!is->hasDataOrRestore(3))
+    if (!is->hasData(3))
       return false;
 
+    is->setRestorePoint();
+
     len = readCompact(is);
-    os->writeOpaque32(len);
 
     if (!is->hasDataOrRestore(len))
       return false;
 
+    os->writeOpaque32(len);
     os->copyBytes(is, len);
 
     is->clearRestorePoint();
 
+    readState = IDLE;
     return true;
   }
 
-  // Quit on unsupported compression type.
-  if (comp_ctl > tightMaxSubencoding)
-    throw protocol_error("TightDecoder: Bad subencoding value received");
-
   // "Basic" compression type.
 
-  int palSize = 0;
-
-  if (r.width() > TIGHT_MAX_WIDTH)
-    throw protocol_error(core::format(
-      "TightDecoder: Too large rectangle (%d pixels)", r.width()));
-
   // Possible palette
-  if ((comp_ctl & tightExplicitFilter) != 0) {
+  if (readState == FILTER) {
     uint8_t filterId;
 
-    if (!is->hasDataOrRestore(1))
+    if (!is->hasData(1))
       return false;
 
     filterId = is->readU8();
@@ -128,37 +142,52 @@ bool TightDecoder::readRect(const core::Rect& r, rdr::InStream* is,
 
     switch (filterId) {
     case tightFilterPalette:
-      if (!is->hasDataOrRestore(1))
-        return false;
-
-      palSize = is->readU8() + 1;
-      os->writeU8(palSize - 1);
-
-      if (server.pf().is888()) {
-        if (!is->hasDataOrRestore(palSize * 3))
-          return false;
-        os->copyBytes(is, palSize * 3);
-      } else {
-        if (!is->hasDataOrRestore(palSize * server.pf().bpp/8))
-          return false;
-        os->copyBytes(is, palSize * server.pf().bpp/8);
-      }
+      readState = PALETTE;
       break;
     case tightFilterGradient:
       if (server.pf().bpp == 8)
         throw protocol_error("TightDecoder: Invalid BPP for gradient filter");
+      readState = PIXELS;
       break;
     case tightFilterCopy:
+      readState = PIXELS;
       break;
     default:
       throw protocol_error("TightDecoder: Unknown filter code received");
     }
   }
 
+  if (readState == PALETTE) {
+    if (!is->hasData(1))
+      return false;
+
+    is->setRestorePoint();
+
+    readPalSize = is->readU8() + 1;
+
+    if (server.pf().is888()) {
+      if (!is->hasDataOrRestore(readPalSize * 3))
+        return false;
+      os->writeU8(readPalSize - 1);
+      os->copyBytes(is, readPalSize * 3);
+    } else {
+      if (!is->hasDataOrRestore(readPalSize * server.pf().bpp/8))
+        return false;
+      os->writeU8(readPalSize - 1);
+      os->copyBytes(is, readPalSize * server.pf().bpp/8);
+    }
+
+    is->clearRestorePoint();
+
+    readState = PIXELS;
+  }
+
+  assert(readState == PIXELS);
+
   size_t rowSize, dataSize;
 
-  if (palSize != 0) {
-    if (palSize <= 2)
+  if (readPalSize != 0) {
+    if (readPalSize <= 2)
       rowSize = (r.width() + 7) / 8;
     else
       rowSize = r.width();
@@ -171,27 +200,30 @@ bool TightDecoder::readRect(const core::Rect& r, rdr::InStream* is,
   dataSize = r.height() * rowSize;
 
   if (dataSize < TIGHT_MIN_TO_COMPRESS) {
-    if (!is->hasDataOrRestore(dataSize))
+    if (!is->hasData(dataSize))
       return false;
     os->copyBytes(is, dataSize);
   } else {
     uint32_t len;
 
     // FIXME: Might be less than 3 bytes
-    if (!is->hasDataOrRestore(3))
+    if (!is->hasData(3))
       return false;
 
+    is->setRestorePoint();
+
     len = readCompact(is);
-    os->writeOpaque32(len);
 
     if (!is->hasDataOrRestore(len))
       return false;
 
+    os->writeOpaque32(len);
     os->copyBytes(is, len);
+
+    is->clearRestorePoint();
   }
 
-  is->clearRestorePoint();
-
+  readState = IDLE;
   return true;
 }
 

common/rfb/TightDecoder.h
@@ -58,6 +58,16 @@ namespace rfb {
                        int stride, const core::Rect& r);
 
   private:
+    enum {
+      IDLE,
+      FILL,
+      JPEG,
+      FILTER,
+      PALETTE,
+      PIXELS,
+    } readState;
+    int readPalSize;
+
     rdr::ZlibInStream zis[4];
   };
 }

common/rfb/ZRLEDecoder.cxx
@@ -89,13 +89,13 @@ bool ZRLEDecoder::readRect(const core::Rect& /*r*/, rdr::InStream* is,
   is->setRestorePoint();
 
   len = is->readU32();
-  os->writeU32(len);
 
   if (!is->hasDataOrRestore(len))
     return false;
 
   is->clearRestorePoint();
 
+  os->writeU32(len);
   os->copyBytes(is, len);
 
   return true;