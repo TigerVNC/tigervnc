cb8238e34325dd2d0fd5026b359072f82aa7292a
Samuel Mannehed <samuel@cendio.se>
2025-10-14 16:44:50 +0200

    Use new event dispatch handler in DesktopWindow
    This rids us of the limit of only one event dispatch handler.

--- vncviewer/DesktopWindow.h
+++ vncviewer/DesktopWindow.h
@@ -92,6 +92,7 @@ private:
   void addOverlay(const char *text);
   static void updateOverlay(void *data);
 
+  static int dispatch_helper(int event, Fl_Window *w, void *data);
   static int fltkDispatch(int event, Fl_Window *win);
   static int fltkHandle(int event);
 
