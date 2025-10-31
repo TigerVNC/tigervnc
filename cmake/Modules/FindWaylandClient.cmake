5a006ee9959e9ab0e47e49c89c505c95bc665218
Adam Halim <adaha@cendio.se>
2025-10-31 09:05:30 +0100

    Fix WaylandClient find module
    CMake will complain if the argument passed to
    find_package_handle_standard_args doesn't match the argument passed to
    the find module. In this case, "Wayland" didn't match "WaylandClient".

--- cmake/Modules/FindWaylandClient.cmake
+++ cmake/Modules/FindWaylandClient.cmake
@@ -1,13 +1,13 @@
 #[=======================================================================[.rst:
-FindWayland
-----------
+FindWaylandClient
+-----------------
 Find the Wayland client library
 Result variables
 ^^^^^^^^^^^^^^^^
 This module will set the following variables if found:
-``WAYLAND_CLIENT_INCLUDE_DIRS``
+``WAYLANDCLIENT_INCLUDE_DIRS``
   where to find wayland-client.h.h, etc.
-``WAYLAND_CLIENT_LIBRARIES``
+``WAYLANDCLIENT_LIBRARIES``
   the libraries to link against to use wayland client.
 ``WAYLAND_CLIENT_FOUND``
   TRUE if found
@@ -16,35 +16,35 @@ This module will set the following variables if found:
 find_package(PkgConfig QUIET)
 
 if (PKG_CONFIG_FOUND)
-  pkg_check_modules(PC_Wayland_client QUIET wayland-client)
+  pkg_check_modules(PC_WaylandClient QUIET wayland-client)
 endif()
 
 find_program(Wayland_scanner_EXECUTABLE
     NAMES wayland-scanner
 )
 
-find_path(Wayland_client_INCLUDE_DIR NAMES wayland-client.h
+find_path(WaylandClient_INCLUDE_DIR NAMES wayland-client.h
   PATH_SUFFIXES
     wayland
   HINTS
-    ${PC_Wayland_client_INCLUDE_DIRS}
+    ${PC_WaylandClient_INCLUDE_DIRS}
 )
-mark_as_advanced(Wayland_client_INCLUDE_DIR)
+mark_as_advanced(WaylandClient_INCLUDE_DIR)
 
-find_library(Wayland_LIBRARIES NAMES wayland-client
+find_library(WaylandClient_LIBRARIES NAMES wayland-client
   HINTS
-    ${PC_Wayland_client_LIBRARY_DIRS}
+    ${PC_WaylandClient_LIBRARY_DIRS}
 )
-mark_as_advanced(Wayland_LIBRARIES)
+mark_as_advanced(WaylandClient_LIBRARIES)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Wayland
+find_package_handle_standard_args(WaylandClient
   REQUIRED_VARS
-    Wayland_LIBRARIES Wayland_client_INCLUDE_DIR Wayland_scanner_EXECUTABLE
+    WaylandClient_LIBRARIES WaylandClient_INCLUDE_DIR Wayland_scanner_EXECUTABLE
 )
 
-if(Wayland_FOUND)
-  set(WAYLAND_INCLUDE_DIRS ${Wayland_client_INCLUDE_DIR})
-  set(WAYLAND_LIBRARIES ${Wayland_LIBRARIES})
+if(WaylandClient_FOUND)
+  set(WAYLANDCLIENT_INCLUDE_DIRS ${WaylandClient_INCLUDE_DIR})
+  set(WAYLANDCLIENT_LIBRARIES ${WaylandClient_LIBRARIES})
   set(WAYLAND_SCANNER_EXECUTABLE ${Wayland_scanner_EXECUTABLE})
 endif()
