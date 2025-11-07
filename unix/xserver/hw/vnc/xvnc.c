1fa572eeace55c0b3797df0ea1fa3c0273632744
Pierre Ossman <ossman@cendio.se>
2025-11-07 10:57:20 +0100

    Change development version to 1.16.80

--- unix/xserver/hw/vnc/xvnc.c
+++ unix/xserver/hw/vnc/xvnc.c
@@ -73,7 +73,7 @@ extern char buildtime[];
 
 #include "version-config.h"
 
-#define XVNCVERSION "TigerVNC 1.15.80"
+#define XVNCVERSION "TigerVNC 1.16.80"
 #define XVNCCOPYRIGHT ("Copyright (C) 1999-2025 TigerVNC team and many others (see README.rst)\n" \
                        "See https://www.tigervnc.org for information on TigerVNC.\n")
 
