36167cb792f28ed3f54150b636e23afec9ce62b9
Adam Halim <adaha@cendio.se>
2026-01-21 14:45:43 +0100

    Disable VirtualKeyboard/Pointer if error
    We can still allow a session, with keyboard/pointer turned off, instead
    of denying a session entirely.

--- unix/w0vncserver/wayland/WaylandDesktop.cxx
+++ unix/w0vncserver/wayland/WaylandDesktop.cxx
@@ -78,8 +78,16 @@ void WaylandDesktop::init(rfb::VNCServer* vs)
 void WaylandDesktop::start()
 {
   std::function<void()> desktopReadyCb = [this]() {
-    virtualPointer = new wayland::VirtualPointer(display, seat);
-    virtualKeyboard = new wayland::VirtualKeyboard(display, seat);
+    try {
+      virtualPointer = new wayland::VirtualPointer(display, seat);
+    } catch (std::exception& e) {
+      vlog.error("%s - pointer will be disabled", e.what());
+    }
+    try {
+      virtualKeyboard = new wayland::VirtualKeyboard(display, seat);
+    } catch (std::exception& e) {
+      vlog.error("%s - keyboard will be disabled", e.what());
+    }
 
     server->setPixelBuffer(pb);
     server->setLEDState(virtualKeyboard->getLEDState());
@@ -115,6 +123,9 @@ void WaylandDesktop::stop()
 
 void WaylandDesktop::pointerEvent(const core::Point& pos, uint16_t buttonMask)
 {
+  if (!virtualPointer)
+    return;
+
   virtualPointer->motionAbsolute(pos.x, pos.y, pb->width(), pb->height());
 
   if (buttonMask == oldButtonMask)
@@ -134,6 +145,9 @@ void WaylandDesktop::pointerEvent(const core::Point& pos, uint16_t buttonMask)
 
 void WaylandDesktop::keyEvent(uint32_t keysym, uint32_t keycode, bool down)
 {
+  if (!virtualKeyboard)
+    return;
+
   virtualKeyboard->key(keysym, keycode, down);
 }
 
