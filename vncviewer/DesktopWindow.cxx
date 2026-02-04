TigerVNC/tigervnc

commit ed0e23a55e70e47511d0c7e24e8b051092fb1276

Use new event dispatch handler in DesktopWindow
This rids us of the limit of only one event dispatch handler.

vncviewer/DesktopWindow.cxx
@@ -46,6 +46,7 @@
 #include "Surface.h"
 #include "Viewport.h"
 #include "touch.h"
+#include "fltk/event_dispatch_handler.h"
 
 #include <FL/Fl.H>
 #include <FL/Fl_Image_Surface.H>
@@ -123,7 +124,7 @@ DesktopWindow::DesktopWindow(int w, int h, CConn* cc_)
   instances.insert(this);
 
   // Hack. See below...
-  Fl::event_dispatch(fltkDispatch);
+  fl_add_event_dispatch(fltkDispatch, this);
 
   // Support for -geometry option. Note that although we do support
   // negative coordinates, we do not support -XOFF-YOFF (ie
@@ -284,7 +285,7 @@ DesktopWindow::~DesktopWindow()
   if (instances.size() == 0)
     Fl::remove_handler(fltkHandle);
 
-  Fl::event_dispatch(Fl::handle_);
+  fl_remove_event_dispatch(fltkDispatch, this);
 
   // FLTK automatically deletes all child widgets, so we shouldn't touch
   // them ourselves here
@@ -990,10 +991,8 @@ int DesktopWindow::handle(int event)
 }
 
 
-int DesktopWindow::fltkDispatch(int event, Fl_Window *win)
+int DesktopWindow::fltkDispatch(int event, Fl_Window *win, void *)
 {
-  int ret;
-
   // FLTK keeps spamming bogus FL_MOVE events if _any_ X event is
   // received with the mouse pointer outside our windows
   // https://github.com/fltk/fltk/issues/76
@@ -1010,8 +1009,6 @@ int DesktopWindow::fltkDispatch(int event, Fl_Window *win)
       return 0;
   }
 #endif
-
-  ret = Fl::handle_(event, win);
 
   DesktopWindow *dw = dynamic_cast<DesktopWindow*>(win);
 
@@ -1051,7 +1048,7 @@ int DesktopWindow::fltkDispatch(int event, Fl_Window *win)
     }
   }
 
-  return ret;
+  return 0;
 }
 
 int DesktopWindow::fltkHandle(int event)

vncviewer/DesktopWindow.h
@@ -92,7 +92,7 @@ private:
   void addOverlay(const char *text);
   static void updateOverlay(void *data);
 
-  static int fltkDispatch(int event, Fl_Window *win);
+  static int fltkDispatch(int event, Fl_Window *win, void *);
   static int fltkHandle(int event);
 
   bool hasFocus();

commit c8fd4eb7dfc2f263d0a10703a749d68ed2c79cb2

Fix dropping bogus focus events
This was partially broken by ed0e23a as the event wasn't properly
dropped and still propagated to widgets.

vncviewer/DesktopWindow.cxx
@@ -1000,7 +1000,7 @@ int DesktopWindow::fltkDispatch(int event, Fl_Window *win, void *)
   if ((event == FL_FOCUS) || (event == FL_UNFOCUS)) {
     const XFocusChangeEvent* xfocus = &fl_xevent->xfocus;
     if ((xfocus->mode == NotifyGrab) || (xfocus->mode == NotifyUngrab))
-      return 0;
+      return 1;
   }
 #endif
 